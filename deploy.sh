ab06bb7bf60efbba8700ad75984723df22ed0032
Florian Lueke <flueke@mesytec.com>
2016-07-14 15:18:39 +0200

    hacking on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# != 2 ] ; then
-    echo "usage $0 PATH_TO_BINARY TARGET_FOLDER"
+if [ $# != 3 ] ; then
+    echo "usage $0 PATH_TO_BINARY PATH_TO_SOURCES TARGET_FOLDER"
     exit 1
 fi
 
 PATH_TO_BINARY="$1"
-TARGET_FOLDER="$2"
+PATH_TO_SOURCES="$2"
+TARGET_FOLDER="$3"
 
 # if we cannot find the the binary we have to abort
 if [ ! -f "$PATH_TO_BINARY" ] ; then
@@ -14,9 +15,13 @@ if [ ! -f "$PATH_TO_BINARY" ] ; then
     exit 1
 fi
 
+if [ ! -d "$PATH_TO_SOURCES" ] ; then
+    echo "The file '$PATH_TO_SOURCES' was not found. Aborting!"
+    exit 1
+fi
+
 # copy the binary to the target folder
 # create directories if required
-echo "---> copy binary itself"
 echo "---> copy binary to target folder"
 cp -v "$PATH_TO_BINARY" "$TARGET_FOLDER" || exit 1
 
@@ -28,3 +33,13 @@ for lib in `ldd "$PATH_TO_BINARY" | cut -d'>' -f2 | awk '{print $1}'` ; do
         cp -v "$lib" "$TARGET_FOLDER"
    fi  
 done
+
+echo "---> copy qt plugins"
+cp -vr /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms "$TARGET_FOLDER"
+cp -vr /usr/lib/x86_64-linux-gnu/qt5/plugins/imageformats "$TARGET_FOLDER"
+
+echo "---> copy auxiliary files"
+cp -v "$PATH_TO_SOURCES/README.txt" "$TARGET_FOLDER"
+cp -v "$PATH_TO_SOURCES/default-initlist.init" "$TARGET_FOLDER"
+cp -v "$PATH_TO_SOURCES/default-stack.stk" "$TARGET_FOLDER"
+cp -v "$PATH_TO_SOURCES/mvme2.sh" "$TARGET_FOLDER"
