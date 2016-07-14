0520dd15bef78fa4f9a82b53ff641e84e8a8804d
Florian Lüke <f.lueke@mesytec.com>
2016-07-14 12:38:31 +0200

    load default stack and initlist from files into gui at startup

--- main.cpp
+++ main.cpp
@@ -5,25 +5,6 @@
 
 int main(int argc, char *argv[])
 {
-
-    QString testInput =
-        "0x1234 10\n"
-        "0x1235 11\r"
-        "0x1236 12 a comment\n"
-        "0x1237 13 # another comment 0xdead\n"
-        "0x1238 14 // and yet another one\n"
-        "42 0x42\n\r"
-        "ending with a comment and no newline 42";
-
-    QTextStream stream(&testInput);
-
-    auto numbers = parseStackFile(stream);
-
-    qDebug() << numbers;
-    qDebug() << numbers.size();
-
-
-
     QApplication a(argc, argv);
 
     QCoreApplication::setOrganizationDomain("www.mesytec.com");
