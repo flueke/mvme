66f9d4332fc052d65407f5185709cf61d771e696
Florian Lüke <f.lueke@mesytec.com>
2017-01-26 13:03:24 +0100

    gui_util: add embellish_pixmap()

--- src/gui_util.cc
+++ src/gui_util.cc
@@ -1,6 +1,8 @@
 #include "gui_util.h"
+#include <QDebug>
 #include <QFile>
 #include <QHBoxLayout>
+#include <QPainter>
 #include <QTextBrowser>
 #include <QTextStream>
 #include <QWidget>
@@ -31,4 +33,14 @@ QWidget *make_vme_script_ref_widget()
     }
 
     return widget;
+}
+
+QPixmap embellish_pixmap(const QString &original_source, const QString &embellishment_source)
+{
+    QPixmap result(original_source);
+    QPixmap embellishment(embellishment_source);
+    QRect target_rect(result.width() / 2, result.height() / 2, result.width() / 2, result.height() / 2);
+    QPainter painter(&result);
+    painter.drawPixmap(target_rect, embellishment, embellishment.rect());
+    return result;
 }
--- src/gui_util.h
+++ src/gui_util.h
@@ -1,8 +1,17 @@
 #ifndef __GUI_UTIL_H__
 #define __GUI_UTIL_H__
 
+#include <QPixmap>
+
 class QWidget;
 
 QWidget *make_vme_script_ref_widget();
+
+/** Paints the pixmap created from embellishment_source onto the pixmap
+ * obtained from original_source. The embellishment is painted into the
+ * bottom-right corner of the source pixmap.
+ * Returns the resulting pixmap.
+ */
+QPixmap embellish_pixmap(const QString &original_source, const QString &embellishment_source);
 
 #endif /* __GUI_UTIL_H__ */
