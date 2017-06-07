d50f4adde9a92313b337e88d9a4a03a66e7f60a4
Florian Lüke <f.lueke@mesytec.com>
2017-06-07 14:04:52 +0200

    Add license header to sources

--- src/globals.h
+++ src/globals.h
@@ -1,3 +1,21 @@
+/* mvme - Mesytec VME Data Acquisition
+ *
+ * Copyright (C) 2016, 2017  Florian Lüke <f.lueke@mesytec.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 #ifndef UUID_6fd8e7d2_5ff5_4908_8b28_fbe474a74ebd
 #define UUID_6fd8e7d2_5ff5_4908_8b28_fbe474a74ebd
 
