9c8a2ee0007d75bee35c49c70ff0a51ab3749fa6
Florian Lüke <f.lueke@mesytec.com>
2017-06-07 13:55:08 +0200

    Fixes to add-license-header.sh

--- add-license-header.sh
+++ add-license-header.sh
@@ -2,12 +2,13 @@
 # From: https://stackoverflow.com/a/151690
 
 FILES=(*.h *.cc)
+INPUTFILE="`dirname $0`/LICENSE-SHORT.TXT"
 
 for i in ${FILES[@]}
 do
     if ! grep -q Copyright $i
     then
         echo $i
-        #cat copyright.txt $i >$i.new && mv $i.new $i
+        cat $INPUTFILE $i >$i.new && mv $i.new $i
     fi
 done
