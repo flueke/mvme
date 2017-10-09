6fff45968f058268e9fa1ab08b0e161edb54a2ce
Florian Lüke <f.lueke@mesytec.com>
2017-10-09 10:12:37 +0200

    DataFilter: benchmark the non-gather case

--- test/bench_data_filter.cc
+++ test/bench_data_filter.cc
@@ -13,6 +13,18 @@ static void BM_DataFilter_gather(benchmark::State &state)
     }
 }
 
+static void BM_DataFilter_no_gather(benchmark::State &state)
+{
+    DataFilter filter(makeFilterFromString("0000 XXXX XXXX XXXX XXAA AAAA DDDD DDDD"));
+    u32 dataWord = 0b111101001110100101;
+
+    while (state.KeepRunning())
+    {
+        u32 result = filter.extractData(dataWord, 'a');
+        benchmark::DoNotOptimize(result);
+    }
+}
+
 static void BM_DataFilterExternalCache_gather(benchmark::State &state)
 {
     DataFilterExternalCache filter("0000 XXXA XXXX XXAA AAAA DDDD DDDD DDDD");
@@ -26,7 +38,21 @@ static void BM_DataFilterExternalCache_gather(benchmark::State &state)
     }
 }
 
+static void BM_DataFilterExternalCache_no_gather(benchmark::State &state)
+{
+    DataFilterExternalCache filter("0000 XXXX XXXX XXXX XXAA AAAA DDDD DDDD");
+    auto fcA = filter.makeCacheEntry('a');
+    u32 dataWord = 0b111101001110100101;
+    while (state.KeepRunning())
+    {
+        u32 result = filter.extractData(dataWord, fcA);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
 BENCHMARK(BM_DataFilter_gather);
+BENCHMARK(BM_DataFilter_no_gather);
 BENCHMARK(BM_DataFilterExternalCache_gather);
+BENCHMARK(BM_DataFilterExternalCache_no_gather);
 
 BENCHMARK_MAIN();
