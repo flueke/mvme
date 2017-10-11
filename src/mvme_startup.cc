61d958073a162d3f32b0e1fb66a96804e8b7f41b
Florian Lüke <f.lueke@mesytec.com>
2017-10-11 11:29:42 +0200

    output build info on startup

--- src/mvme_startup.cc
+++ src/mvme_startup.cc
@@ -11,6 +11,7 @@
 #ifdef MVME_USE_GIT_VERSION_FILE
 #include "git_sha1.h"
 #endif
+#include "build_info.h"
 
 void mvme_basic_init(const QString &appName)
 {
@@ -29,4 +30,7 @@ void mvme_basic_init(const QString &appName)
     qDebug() << "prefixPath = " << QLibraryInfo::location(QLibraryInfo::PrefixPath);
     qDebug() << "librariesPaths = " << QLibraryInfo::location(QLibraryInfo::LibrariesPath);
     qDebug() << "pluginsPaths = " << QLibraryInfo::location(QLibraryInfo::PluginsPath);
+    qDebug() << "GIT_VERSION =" << GIT_VERSION;
+    qDebug() << "BUILD_TYPE =" << BUILD_TYPE;
+    qDebug() << "BUILD_CXX_FLAGS =" << BUILD_CXX_FLAGS;
 }
