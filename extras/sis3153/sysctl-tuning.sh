d93969c2b4ded76687a24f4a1adab8d20521a4f5
Florian Lüke <f.lueke@mesytec.com>
2017-11-30 16:16:05 +0100

    add netdev_max_backlog setting to sis tuning script

--- extras/sis3153/sysctl-tuning.sh
+++ extras/sis3153/sysctl-tuning.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
+
 # Network TCP/UDP tuning to support high-bandwith applications
 #
 sysctl -w net.core.rmem_max=8388608
 sysctl -w net.core.wmem_max=8388608
 sysctl -w net.core.rmem_default=65536
 sysctl -w net.core.wmem_default=65536
-#
+sysctl -w net.core.netdev_max_backlog=2000
+
+# udp
 sysctl -w net.ipv4.udp_mem='8388608 8388608 8388608'
-#
+
+# tcp
 sysctl -w net.ipv4.tcp_rmem='4096 87380 8388608'
 sysctl -w net.ipv4.tcp_wmem='4096 65536 8388608'
 sysctl -w net.ipv4.tcp_mem='8388608 8388608 8388608'
