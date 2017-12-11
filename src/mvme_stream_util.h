6281dd262e8d9d8845c24a0e48aff7f5ab114bda
Florian Lüke <f.lueke@mesytec.com>
2017-12-11 16:43:16 +0100

    change MVMEStreamWriter to use error codes instead of exceptions

--- src/mvme_stream_util.h
+++ src/mvme_stream_util.h
@@ -7,25 +7,24 @@
 #include "databuffer.h"
 #include "mvme_listfile.h"
 
-struct MVMEStreamWriterError: public std::runtime_error
-{
-    MVMEStreamWriterError(const char *s) : std::runtime_error(s) {}
-};
-
-struct MVMEStreamWriterLogicError: public MVMEStreamWriterError
-{
-    MVMEStreamWriterLogicError(const char *s) : MVMEStreamWriterError(s) {}
-};
-
-struct MVMEStreamWriterSizeError: public MVMEStreamWriterError
-{
-    MVMEStreamWriterSizeError(const char *s) : MVMEStreamWriterError(s) {}
-};
-
 class MVMEStreamWriterHelper
 {
     public:
         using LF = listfile_v1;
+
+        enum Flags
+        {
+            ResultOk,
+            EventSizeExceeded,
+            ModuleSizeExceeded,
+            NestingError
+        };
+
+        struct CloseResult
+        {
+            Flags flags = ResultOk;
+            u32 sectionSize = 0;
+        };
 
         MVMEStreamWriterHelper(DataBuffer *outputBuffer = nullptr)
             : m_outputBuffer(outputBuffer)
@@ -47,15 +46,12 @@ class MVMEStreamWriterHelper
         inline s32 eventHeaderOffset() const { return m_eventHeaderOffset; }
         inline s32 moduleHeaderOffset() const { return m_moduleHeaderOffset; }
 
-        inline u32 *openEventSection(int eventIndex)
+        inline Flags openEventSection(int eventIndex)
         {
             assert(m_outputBuffer);
 
-            if (hasOpenEventSection())
-                throw MVMEStreamWriterLogicError("Cannot open new event section while event section is open");
-
-            if (hasOpenModuleSection())
-                throw MVMEStreamWriterLogicError("Cannot open new event section while module section is open");
+            if (hasOpenEventSection() || hasOpenModuleSection())
+                return NestingError;
 
             m_eventHeaderOffset = m_outputBuffer->used;
             u32 *eventHeader = m_outputBuffer->asU32();
@@ -65,37 +61,32 @@ class MVMEStreamWriterHelper
             *eventHeader = ((ListfileSections::SectionType_Event << LF::SectionTypeShift) & LF::SectionTypeMask)
                 | ((eventIndex << LF::EventTypeShift) & LF::EventTypeMask);
 
-            return eventHeader;
+            return ResultOk;
         }
 
-        inline u32 closeEventSection()
+        inline CloseResult closeEventSection()
         {
             assert(m_outputBuffer);
 
-            if (!hasOpenEventSection())
-                throw MVMEStreamWriterLogicError("Cannot close event section: no event section open");
-
-            if (hasOpenModuleSection())
-                throw MVMEStreamWriterLogicError("Cannot close event section: module section is open");
+            if (!hasOpenEventSection() || hasOpenModuleSection())
+                return { NestingError, 0 };
 
             u32 *eventHeader = m_outputBuffer->asU32(m_eventHeaderOffset);
             *eventHeader |= (m_eventSize << LF::SectionSizeShift) & LF::SectionSizeMask;
             m_eventHeaderOffset = -1;
-            return m_eventSize;
+
+            return { ResultOk, m_eventSize };
         }
 
-        inline u32 *openModuleSection(u32 moduleType)
+        inline Flags openModuleSection(u32 moduleType)
         {
             assert(m_outputBuffer);
 
-            if (!hasOpenEventSection())
-                throw MVMEStreamWriterLogicError("Cannot open module section without open event section");
-
-            if (hasOpenModuleSection())
-                throw MVMEStreamWriterLogicError("Cannot open new module section while module section is open");
+            if (!hasOpenEventSection() || hasOpenModuleSection())
+                return NestingError;
 
             if (m_eventSize >= LF::SectionMaxWords)
-                throw MVMEStreamWriterSizeError("Cannot open module section: event section size exceeded");
+                return EventSizeExceeded;
 
             m_moduleHeaderOffset = m_outputBuffer->used;
             u32 *moduleHeader = m_outputBuffer->asU32();
@@ -105,63 +96,59 @@ class MVMEStreamWriterHelper
 
             *moduleHeader = (moduleType << LF::ModuleTypeShift) & LF::ModuleTypeMask;
 
-            return moduleHeader;
+            return ResultOk;
         }
 
-        inline u32 closeModuleSection()
+        inline CloseResult closeModuleSection()
         {
             assert(m_outputBuffer);
 
-            if (!hasOpenEventSection())
-                throw MVMEStreamWriterLogicError("Cannot close module section: no event section open");
-
-            if (!hasOpenModuleSection())
-                throw MVMEStreamWriterLogicError("Cannot close module section: no module section open");
+            if (!hasOpenEventSection() || !hasOpenModuleSection())
+                return { NestingError, 0 };
 
             u32 *moduleHeader = m_outputBuffer->asU32(m_moduleHeaderOffset);
             *moduleHeader |= (m_moduleSize << LF::SubEventSizeShift) & LF::SubEventSizeMask;
             m_moduleHeaderOffset = -1;
-            return m_moduleSize;
+
+            return { ResultOk, m_moduleSize };
         }
 
-        inline void writeEventData(u32 dataWord)
+        inline Flags writeEventData(u32 dataWord)
         {
             assert(m_outputBuffer);
 
-            if (!hasOpenEventSection())
-                throw MVMEStreamWriterLogicError("Cannot write event data: no event section open");
-
-            if (hasOpenModuleSection())
-                throw MVMEStreamWriterLogicError("Cannot write event data: a module section open");
+            if (!hasOpenEventSection() || hasOpenModuleSection())
+                return NestingError;
 
             if (m_eventSize >= LF::SectionMaxWords)
-                throw MVMEStreamWriterSizeError("Cannot write event data: event section size exceeded");
+                return EventSizeExceeded;
 
             *m_outputBuffer->asU32() = dataWord;
             m_outputBuffer->used += sizeof(u32);
             m_eventSize++;
+
+            return ResultOk;
         }
 
-        inline void writeModuleData(u32 dataWord)
+        inline Flags writeModuleData(u32 dataWord)
         {
             assert(m_outputBuffer);
 
-            if (!hasOpenEventSection())
-                throw MVMEStreamWriterLogicError("Cannot write module data: no event section open");
-
-            if (!hasOpenModuleSection())
-                throw MVMEStreamWriterLogicError("Cannot write module data: no module section open");
+            if (!hasOpenEventSection() || !hasOpenModuleSection())
+                return NestingError;
 
             if (m_eventSize >= LF::SectionMaxWords)
-                throw MVMEStreamWriterSizeError("Cannot write module data: event section size exceeded");
+                return EventSizeExceeded;
 
             if (m_moduleSize >= LF::SubEventMaxWords)
-                throw MVMEStreamWriterSizeError("Cannot write module data: module section size exceeded");
+                return ModuleSizeExceeded;
 
             *m_outputBuffer->asU32() = dataWord;
             m_outputBuffer->used += sizeof(u32);
             m_eventSize++;
             m_moduleSize++;
+
+            return ResultOk;
         }
 
         inline bool hasOpenEventSection() const { return m_eventHeaderOffset >= 0; }
