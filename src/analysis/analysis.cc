d764b334253d5d28d0cab508d33ce0bb65daac71
Florian Lüke <f.lueke@mesytec.com>
2017-12-12 16:12:27 +0100

    disable shared histo mem due to crash

--- src/analysis/analysis.cc
+++ src/analysis/analysis.cc
@@ -30,7 +30,7 @@
 
 #define ENABLE_ANALYSIS_DEBUG 0
 
-#define ANALYSIS_USE_SHARED_HISTO1D_MEM 1
+#define ANALYSIS_USE_SHARED_HISTO1D_MEM 0
 
 template<typename T>
 QDebug &operator<< (QDebug &dbg, const std::shared_ptr<T> &ptr)
