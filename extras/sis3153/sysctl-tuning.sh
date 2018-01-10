e076bd09dc65644e17de2f5d7fe532b211545812
Florian Lüke <f.lueke@mesytec.com>
2018-01-10 11:47:36 +0100

    sis tuning: lower udp_rmem_min to a reasonable value

--- extras/sis3153/sysctl-tuning.sh
+++ extras/sis3153/sysctl-tuning.sh
@@ -31,8 +31,8 @@ sysctl -w net.ipv4.udp_mem='8388608 8388608 8388608'
 # Minimal size of receive buffer used by UDP sockets in moderation.
 # Each UDP socket is able to use the size for receiving data, even if
 # total pages of UDP sockets exceed udp_mem pressure. The unit is byte.
-# Default: 1 page
-sysctl -w net.ipv4.udp_rmem_min=10485760
+# Default: 1 page (4096 bytes)
+sysctl -w net.ipv4.udp_rmem_min=409600
 
 # Apply the above settings now
 sysctl -w net.ipv4.route.flush=1
