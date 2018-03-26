8af360808d9c22a097bad74ccfcdd3ff01f185bc
Florian Lüke <f.lueke@mesytec.com>
2018-03-26 14:56:18 +0200

    fix zstr error handling

--- 3rdparty/zstr/src/strict_fstream.hpp
+++ 3rdparty/zstr/src/strict_fstream.hpp
@@ -36,7 +36,7 @@ static std::string strerror()
 #else
 // GNU-specific strerror_r()
     auto p = strerror_r(errno, &buff[0], buff.size());
-    std::string tmp(p, std::strlen(p));
+    std::string tmp(p, std::strlen(p) + 1);
     std::swap(buff, tmp);
 #endif
     buff.resize(buff.find('\0'));
