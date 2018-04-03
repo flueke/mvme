4bec80a95fa0135bf6b746f315cdc596920bb44d
Florian Lüke <florianlueke@gmx.net>
2018-04-03 22:07:05 +0200

    more

--- src/mvme_exprtk.cc
+++ src/mvme_exprtk.cc
@@ -8,7 +8,11 @@ bool SymbolTable::add_scalar(const std::string &name, double &value)
     if (entries.count(name))
         return false;
 
-    entries[name] = Entry{ Entry::Scalar, &value, nullptr, nullptr };
+    Entry entry;
+    entry.type   = Entry::Scalar;
+    entry.scalar = &value;
+
+    entries[name] = entry;
     return true;
 }
 
@@ -16,6 +20,10 @@ bool SymbolTable::add_string(const std::string &name, std::string &str)
 {
     if (entries.count(name))
         return false;
+
+    Entry entry;
+    entry.type   = Entry::String;
+    entry.string = &str;
 
     entries[name] = Entry{ Entry::String, nullptr, &str, nullptr };
     return true;
@@ -25,6 +33,10 @@ bool SymbolTable::add_vector(const std::string &name, std::vector<double> &vec)
 {
     if (entries.count(name))
         return false;
+
+    Entry entry;
+    entry.type   = Entry::Scalar;
+    entry.scalar = &value;
 
     entries[name] = Entry{ Entry::Vector, nullptr, nullptr, &vec };
     return true;
