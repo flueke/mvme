8b9cebefd64239d2bb276b9f5dd187a4c54ab108
Florian Lüke <f.lueke@mesytec.com>
2018-04-24 16:48:48 +0200

    make Qt signals using Qt::Axis datatype work

--- src/mvme_session.cc
+++ src/mvme_session.cc
@@ -22,6 +22,7 @@ void mvme_init(const QString &appName)
     qRegisterMetaType<GlobalMode>("GlobalMode");
     qRegisterMetaType<MVMEStreamWorkerState>("MVMEStreamWorkerState");
     qRegisterMetaType<ControllerState>("ControllerState");
+    qRegisterMetaType<Qt::Axis>("Qt::Axis");
 
     QCoreApplication::setOrganizationDomain("www.mesytec.com");
     QCoreApplication::setOrganizationName("mesytec");
