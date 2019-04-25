bb2add4499fddf488b9ea0c361038cef50531b7d
Florian Lüke <f.lueke@mesytec.com>
2019-04-25 14:20:09 +0200

    threading: use ticket based mutexes to avoid thread starvation

--- src/mvlc/mvlc_threading.h
+++ src/mvlc/mvlc_threading.h
@@ -2,6 +2,7 @@
 #define __MVLC_THREADING_H__
 
 #include <cassert>
+#include <condition_variable>
 #include <mutex>
 #include "mvlc/mvlc_constants.h"
 
@@ -10,8 +11,57 @@ namespace mesytec
 namespace mvlc
 {
 
-using Mutex = std::mutex;
-using LockGuard = std::lock_guard<Mutex>;
+// Ticket mutex system based on
+// https://stackoverflow.com/questions/6449732/fair-critical-section-linux/6453925#6453925
+
+class TicketMutex
+{
+    public:
+        void lock()
+        {
+            UniqueLock lock(m_mutex);
+            TicketType my_ticket_number = m_queue_tail++;
+
+            while (my_ticket_number != m_queue_head)
+                m_cond.wait(lock);
+        }
+
+        void unlock()
+        {
+            {
+                UniqueLock lock(m_mutex);
+                m_queue_head++;
+            }
+            m_cond.notify_all();
+        }
+
+        bool try_lock()
+        {
+            UniqueLock lock(m_mutex, std::try_to_lock);
+
+            if (lock)
+            {
+                if (m_queue_head == m_queue_tail)
+                {
+                    m_queue_tail++;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    private:
+        using UniqueLock = std::unique_lock<std::mutex>;
+        using TicketType = u16;
+
+        std::condition_variable m_cond;
+        std::mutex m_mutex;
+        TicketType m_queue_head = 0; // current ticket number
+        TicketType m_queue_tail = 0; // next ticket number to take
+};
+
+using Mutex = TicketMutex;
 using UniqueLock = std::unique_lock<Mutex>;
 using LockPair = std::pair<UniqueLock, UniqueLock>;
 
@@ -46,6 +96,7 @@ class Locks
         Mutex m_cmdMutex;
         Mutex m_dataMutex;
 };
+
 
 } // end namespace mvlc
 } // end namespace mesytec
