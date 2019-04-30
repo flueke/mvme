fa825a40c78f79178085537005f02d386093f2fc
Florian Lüke <f.lueke@mesytec.com>
2019-04-30 11:43:25 +0200

    implement VMEController interface for MVLC

---
+++ src/mvlc/mvlc_vme_controller.cc
@@ -0,0 +1,136 @@
+#include "mvlc/mvlc_vme_controller.h"
+
+namespace mesytec
+{
+namespace mvlc
+{
+
+MVLC_VMEController::MVLC_VMEController(MVLCObject *mvlc, QObject *parent)
+    : VMEController(parent)
+    , m_mvlc(mvlc)
+{
+    assert(m_mvlc);
+    connect(m_mvlc, &MVLCObject::stateChanged, this, &MVLC_VMEController::onMVLCStateChanged);
+}
+
+void MVLC_VMEController::onMVLCStateChanged(const MVLCObject::State &oldState,
+                                            const MVLCObject::State &newState)
+{
+    switch (newState)
+    {
+        case MVLCObject::Disconnected:
+            emit controllerClosed();
+            emit controllerStateChanged(ControllerState::Disconnected);
+            break;
+
+        case MVLCObject::Connected:
+            emit controllerOpened();
+            emit controllerStateChanged(ControllerState::Connected);
+            break;
+
+        case MVLCObject::Connecting:
+            emit controllerStateChanged(ControllerState::Connecting);
+            break;
+    }
+}
+
+bool MVLC_VMEController::isOpen() const
+{
+    return m_mvlc->isConnected();
+}
+
+VMEError MVLC_VMEController::open()
+{
+    return m_mvlc->connect();
+}
+
+VMEError MVLC_VMEController::close()
+{
+    return m_mvlc->disconnect();
+}
+
+ControllerState MVLC_VMEController::getState() const
+{
+    switch (m_mvlc->getState())
+    {
+        case MVLCObject::Disconnected:
+            return ControllerState::Disconnected;
+
+        case MVLCObject::Connecting:
+            return ControllerState::Connecting;
+
+        case MVLCObject::Connected:
+            return ControllerState::Connected;
+    }
+
+    return ControllerState::Disconnected;
+}
+
+QString MVLC_VMEController::getIdentifyingString() const
+{
+    switch (getType())
+    {
+        case VMEControllerType::MVLC_USB:
+            return "MVLC_USB";
+
+        case VMEControllerType::MVLC_ETH:
+            return "MVLC_ETH";
+
+        default:
+            break;
+    }
+
+    assert(!"invalid vme controller type");
+    return "<invalid_vme_controller_type>";
+}
+
+VMEControllerType MVLC_VMEController::getType() const
+{
+    switch (m_mvlc->connectionType())
+    {
+        case ConnectionType::USB:
+            return VMEControllerType::MVLC_USB;
+
+        case ConnectionType::UDP:
+            return VMEControllerType::MVLC_ETH;
+    }
+
+    InvalidCodePath;
+    return VMEControllerType::MVLC_USB;
+}
+
+VMEError MVLC_VMEController::write32(u32 address, u32 value, u8 amod)
+{
+    return m_mvlc->vmeSingleWrite(address, value, amod, VMEDataWidth::D32);
+}
+
+VMEError MVLC_VMEController::write16(u32 address, u16 value, u8 amod)
+{
+    return m_mvlc->vmeSingleWrite(address, value, amod, VMEDataWidth::D16);
+}
+
+
+VMEError MVLC_VMEController::read32(u32 address, u32 *value, u8 amod)
+{
+    return m_mvlc->vmeSingleRead(address, *value, amod, VMEDataWidth::D32);
+}
+
+VMEError MVLC_VMEController::read16(u32 address, u16 *value, u8 amod)
+{
+    u32 tmpVal = 0u;
+    auto ec = m_mvlc->vmeSingleRead(address, tmpVal, amod, VMEDataWidth::D16);
+    *value = tmpVal;
+    return ec;
+}
+
+
+VMEError MVLC_VMEController::blockRead(u32 address, u32 transfers,
+                                       QVector<u32> *dest, u8 amod, bool fifo)
+{
+    // TODO: can FIFO mode really by ignored by the mvlc?
+    return m_mvlc->vmeBlockRead(address, amod, transfers, *dest);
+}
+
+
+} // end namespace mvlc
+} // end namespace mesytec
--- src/mvlc/mvlc_vme_controller.h
+++ src/mvlc/mvlc_vme_controller.h
@@ -1,6 +1,55 @@
 #ifndef __MVME_MVLC_VME_CONTROLLER_H__
 #define __MVME_MVLC_VME_CONTROLLER_H__
 
-/* Implementation of the VMEController interface for the MVLC. */
+#include "vme_controller.h"
+#include "mvlc/mvlc_qt_object.h"
+
+// Implementation of the VMEController interface for the MVLC.
+
+namespace mesytec
+{
+namespace mvlc
+{
+
+class MVLC_VMEController: public VMEController
+{
+    Q_OBJECT
+    public:
+        MVLC_VMEController(MVLCObject *mvlc, QObject *parent = nullptr);
+
+        //
+        // VMEController implementation
+        //
+        bool isOpen() const override;
+        VMEError open() override;
+        VMEError close() override;
+        ControllerState getState() const override;
+        QString getIdentifyingString() const override;
+        VMEControllerType getType() const override;
+
+        VMEError write32(u32 address, u32 value, u8 amod) override;
+        VMEError write16(u32 address, u16 value, u8 amod) override;
+
+        VMEError read32(u32 address, u32 *value, u8 amod) override;
+        VMEError read16(u32 address, u16 *value, u8 amod) override;
+
+        VMEError blockRead(u32 address, u32 transfers,
+                           QVector<u32> *dest, u8 amod, bool fifo) override;
+
+        //
+        // MVLC specific methods
+        //
+        MVLCObject *getMVLCObject() { return m_mvlc; }
+
+    private slots:
+        void onMVLCStateChanged(const MVLCObject::State &oldState,
+                                const MVLCObject::State &newState);
+
+    private:
+        MVLCObject *m_mvlc;
+};
+
+} // end namespace mvlc
+} // end namespace mesytec
 
 #endif /* __MVME_MVLC_VME_CONTROLLER_H__ */
