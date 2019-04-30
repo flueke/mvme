b1d53cad969a5bd4ef5ee6bf55298458049367d9
Florian Lüke <f.lueke@mesytec.com>
2019-04-30 11:44:11 +0200

    add MVLC to VMEControllerFactory

--- src/vme_controller_factory.cc
+++ src/vme_controller_factory.cc
@@ -7,6 +7,10 @@
 #include "vmusb.h"
 #include "vmusb_readout_worker.h"
 
+#include "mvlc/mvlc_impl_factory.h"
+#include "mvlc/mvlc_qt_object.h"
+#include "mvlc/mvlc_vme_controller.h"
+
 //
 // VMEControllerFactory
 //
@@ -17,6 +21,19 @@ VMEControllerFactory::VMEControllerFactory(VMEControllerType type)
 
 VMEController *VMEControllerFactory::makeController(const QVariantMap &settings)
 {
+    auto make_mvlc_ctrl = [](std::unique_ptr<mesytec::mvlc::AbstractImpl> impl)
+    {
+        auto obj  = std::make_unique<mesytec::mvlc::MVLCObject>(std::move(impl));
+        auto ctrl = std::make_unique<mesytec::mvlc::MVLC_VMEController>(obj.get());
+
+        // Transfer ownership of the MVLCObject to the newly created
+        // MVLC_VMEController
+        obj->setParent(ctrl.get());
+        obj.release();
+
+        return ctrl.release();
+    };
+
     switch (m_type)
     {
         case VMEControllerType::VMUSB:
@@ -29,6 +46,21 @@ VMEController *VMEControllerFactory::makeController(const QVariantMap &settings)
                 auto result = new SIS3153;
                 result->setAddress(settings["hostname"].toString());
                 return result;
+            } break;
+
+        case VMEControllerType::MVLC_USB:
+            {
+                // TODO: support index and serial number based mvlc usb connetions
+                auto impl = mesytec::mvlc::make_mvlc_usb();
+                return make_mvlc_ctrl(std::move(impl));
+
+            } break;
+
+        case VMEControllerType::MVLC_ETH:
+            {
+                auto hostname = settings["hostname"].toString();
+                auto impl = mesytec::mvlc::make_mvlc_udp(hostname.toStdString().c_str());
+                return make_mvlc_ctrl(std::move(impl));
             } break;
     }
 
@@ -64,7 +96,6 @@ VMEReadoutWorker *VMEControllerFactory::makeReadoutWorker()
 
         case VMEControllerType::SIS3153:
             {
-                //return new SIS3153ReadoutWorkerIRQPolling;
                 return new SIS3153ReadoutWorker;
             } break;
     }
