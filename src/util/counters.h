5104eca979bf5138bd24de8a7eb610108628d665
Florian Lüke <f.lueke@mesytec.com>
2019-07-10 11:05:39 +0200

    improve counter rate calc functions

--- src/util/counters.h
+++ src/util/counters.h
@@ -13,16 +13,16 @@ T calc_delta0(T cur, T prev)
     return cur - prev;
 }
 
-template<typename T>
-T calc_deltas0(const T &cur, const T &prev)
+template<typename Container>
+Container calc_deltas0(const Container &cur, const Container &prev)
 {
     assert(cur.size() == prev.size());
 
-    T result;
+    Container result;
     result.reserve(cur.size());
 
     std::transform(cur.begin(), cur.end(), prev.begin(), std::back_inserter(result),
-                   calc_delta0<typename T::value_type>);
+                   calc_delta0<typename Container::value_type>);
 
     return result;
 }
@@ -38,18 +38,40 @@ double calc_rate0(const T &cur, const T&prev, double dt)
     return rate;
 }
 
-// Returns a pair of formatted (delta, rate) strings.
-template<typename T>
-std::pair<QString, QString> format_delta_and_rate(
-    T counter, T prevCounter, double dt_s,
-    QString unitLabel, UnitScaling unitScaling = UnitScaling::Decimal)
-{
-    T delta = calc_delta0(counter, prevCounter);
-    double rate = calc_rate0(counter, prevCounter, dt_s);
+// From https://stackoverflow.com/a/32119279
+#define TYPE_AND_VAL(foo) decltype(foo), foo
 
-    return std::make_pair(
-        format_number(delta, unitLabel, unitScaling),
-        format_number(rate, unitLabel + QStringLiteral("/s"), unitScaling));
+// Templated version of calc_rate0() to be used like
+// calc_rate0<TYPE_AND_VAL(&MyCounters::fooCounter)>(curCounters, prevCounters, dt);
+template<typename Member, Member member, typename Base>
+double calc_rate0(const Base &cur, const Base &prev, double dt)
+{
+    double ret = calc_rate0(cur.*member, prev.*member, dt);
+    return ret;
+};
+
+// Calculate rates from two sequences of current and previous values. The
+// results are stored and returned in a container of type ResultType.
+// Usage example:
+// auto rates = calc_rates0<std::vector<double>>(
+//      counters.cbegin(), counters.cend(), prevCounters.cbegin(), dt)
+template<typename ResultType, typename InputIterator>
+ResultType calc_rates0(InputIterator curBegin, InputIterator curEnd,
+                       InputIterator prevBegin,
+                       double dt)
+{
+    ResultType result;
+
+    using ValueType = typename std::iterator_traits<InputIterator>::value_type;
+
+    auto calc_func = [dt] (const ValueType &curVal, const ValueType &prevVal)
+    {
+        return calc_rate0(curVal, prevVal, dt);
+    };
+
+    std::transform(curBegin, curEnd, prevBegin, std::back_inserter(result), calc_func);
+
+    return result;
 }
 
 #endif /* __MVME_UTIL_COUNTERS_H__ */
