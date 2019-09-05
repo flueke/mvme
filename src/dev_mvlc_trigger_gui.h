239a75dd96971b8a165d522bf753bdfa267a75fb
Florian Lüke <f.lueke@mesytec.com>
2019-09-05 16:14:39 +0200

    start work on a QGraphicsView based MVLC trigger IO setup application
    This is still in the exploration stages.

---
+++ src/dev_mvlc_trigger_gui.h
@@ -0,0 +1,12 @@
+#ifndef __MVME_DEV_MVLC_TRIGGER_GUI_H__
+#define __MVME_DEV_MVLC_TRIGGER_GUI_H__
+
+#include <QGraphicsView>
+
+class TriggerIOView: public QGraphicsView
+{
+    public:
+
+};
+
+#endif /* __MVME_DEV_MVLC_TRIGGER_GUI_H__ */
