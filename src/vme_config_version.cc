484f9320d2d606b56424a4e1fffdacf274df25c4
Florian Lüke <f.lueke@mesytec.com>
2020-01-27 10:34:05 +0100

    separate function to get the current vme config version number

---
+++ src/vme_config_version.cc
@@ -0,0 +1,8 @@
+#include "vme_config_version.h"
+
+static const int CurrentVMEConfigVersion = 4;
+
+int GetCurrentVMEConfigVersion()
+{
+    return CurrentVMEConfigVersion;
+}
---
+++ src/vme_config_version.h
@@ -0,0 +1,7 @@
+#ifndef __MVME_VME_CONFIG_VERSION_H__
+#define __MVME_VME_CONFIG_VERSION_H__
+
+// Versioning of the DAQ config in case incompatible changes need to be made.
+int GetCurrentVMEConfigVersion();
+
+#endif /* __MVME_VME_CONFIG_VERSION_H__ */
