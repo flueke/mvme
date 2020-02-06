c6dfca9d807c847c14494d3f8f571e0a18102744
Florian Lüke <f.lueke@mesytec.com>
2020-02-06 21:53:39 +0100

    formatting only commit

--- src/util/qt_logview.h
+++ src/util/qt_logview.h
@@ -7,6 +7,7 @@
 
 static const size_t LogViewMaximumBlockCount = 10 * 1000u;
 
-std::unique_ptr<QPlainTextEdit> LIBMVME_EXPORT make_logview(size_t maxBlockCount = LogViewMaximumBlockCount);
+std::unique_ptr<QPlainTextEdit> LIBMVME_EXPORT make_logview(
+    size_t maxBlockCount = LogViewMaximumBlockCount);
 
 #endif /* __MVME_UTIL_QT_LOGVIEW_H__ */
