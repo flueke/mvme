e45ba50ce526070635a384b4941b80e663876945
Florian Lüke <f.lueke@mesytec.com>
2020-12-07 16:35:42 +0100

    keep ListfilterExtractors in execution order in the UI

--- src/analysis/ui_eventwidget.cc
+++ src/analysis/ui_eventwidget.cc
@@ -2076,31 +2076,47 @@ void EventWidgetPrivate::populateDataSourceTree(
         {
             QObject::disconnect(mod, &ConfigObject::modified, m_q, &EventWidget::repopulate);
             QObject::connect(mod, &ConfigObject::modified, m_q, &EventWidget::repopulate);
+
             auto moduleNode = make_module_node(mod);
             eventNode->addChild(moduleNode);
             moduleNode->setExpanded(m_expandedObjects[TreeType_Operator].contains(mod));
 
+            // Separate ListFilterExtractors from other data sources, then add
+            // the listfilters in the order they're executed by the analysis.
+            // The other extractors are sorted and added after the listfilters.
+            auto is_listfilter = [] (const auto &a)
+            {
+                return qobject_cast<const ListFilterExtractor *>(a.get());
+            };
+
             auto sources = analysis->getSourcesByModule(mod->getId());
+
+            SourceVector listfilters;
+
+            std::copy_if(
+                std::begin(sources), std::end(sources),
+                std::back_inserter(listfilters), is_listfilter);
+
+            sources.erase(std::remove_if(
+                    std::begin(sources), std::end(sources), is_listfilter),
+                std::end(sources));
+
             std::sort(std::begin(sources), std::end(sources), qobj_ptr_natural_compare);
 
-#if 0
-            //#ifndef QT_NO_DEBUG
-            qDebug() << __PRETTY_FUNCTION__ << ">>>>> sources in order:";
-            for (auto source: sources)
-            {
-                qDebug() << source.get();
-            }
-            qDebug() << __PRETTY_FUNCTION__ << " <<<< end sources";
-#endif
-
-            for (auto source: sources)
+            auto add_node = [this, moduleNode] (const auto &source)
             {
                 auto sourceNode = make_datasource_node(source.get());
                 moduleNode->addChild(sourceNode);
 
                 assert(m_objectMap.count(source) == 0);
                 m_objectMap[source] = sourceNode;
-            }
+            };
+
+            for (auto source: listfilters)
+                add_node(source);
+
+            for (auto source: sources)
+                add_node(source);
         }
     }
 
