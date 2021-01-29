900c92824ecdf0ef7dc4998dcb94a61694203b56
Florian Lüke <f.lueke@mesytec.com>
2021-01-29 13:07:42 +0100

    trigger_io: shorten Counter0 name again

--- src/mvlc/mvlc_trigger_io.cc
+++ src/mvlc/mvlc_trigger_io.cc
@@ -305,7 +305,7 @@ const std::array<QString, trigger_io::Level3::UnitCount+1> Level3::DefaultUnitNa
     "MasterTrigger1",
     "MasterTrigger2",
     "MasterTrigger3",
-    "Counter0+Timestamper",
+    "Counter0",
     "Counter1",
     "Counter2",
     "Counter3",
