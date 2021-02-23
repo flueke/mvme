0a7d77fbae6e50c5b32f2925f58f11fc9684cbbb
Florian Lüke <f.lueke@mesytec.com>
2021-02-23 11:16:58 +0100

    readd simulated trace to trace selection tree

--- src/mvlc/trigger_io_dso_sim_ui.cc
+++ src/mvlc/trigger_io_dso_sim_ui.cc
@@ -193,7 +193,6 @@ std::unique_ptr<TraceTreeModel> make_trace_tree_model()
         samplesRoot->appendRow(row);
     }
 
-#if 0 // XXX: disabled for the next mvme beta release
     // L0
     auto l0Root = make_non_trace_item("L0");
     root->appendRow(l0Root);
@@ -272,7 +271,6 @@ std::unique_ptr<TraceTreeModel> make_trace_tree_model()
         UnitAddress unit = { 3, i+Level3::ECL_Unit_Offset, 0 };
         l3OutRoot->appendRow(make_trace_row({ unit, PinPosition::Output }));
     }
-#endif
 
     // Finalize
     model->setHeaderData(0, Qt::Horizontal, "Trace");
