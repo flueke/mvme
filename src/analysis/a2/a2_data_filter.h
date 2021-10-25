61d2c49d80d36e52f57f4660a26e51dc4906dc4f
Florian Lüke <f.lueke@mesytec.com>
2021-10-25 20:23:49 +0200

    port the code to the new DataFilter location in the mesytec-mvlc lib

--- src/analysis/a2/a2_data_filter.h
+++ src/analysis/a2/a2_data_filter.h
@@ -21,6 +21,7 @@
 #ifndef __A2_DATA_FILTER__
 #define __A2_DATA_FILTER__
 
+#include <mesytec-mvlc/util/data_filter.h>
 #include "util/bits.h"
 
 #include <string>
@@ -36,84 +37,17 @@ namespace a2
 namespace data_filter
 {
 
-static const s32 FilterSize = 32;
-
-struct DataFilter
-{
-    std::array<char, FilterSize> filter;
-    u32 matchMask  = 0;
-    u32 matchValue = 0;
-    s32 matchWordIndex = -1;
-
-    // bool operator==(const DataFilter &o) const = default; // c++20
-    inline bool operator==(const DataFilter &o) const
-    {
-        return (filter == o.filter
-                && matchMask == o.matchMask
-                && matchValue == o.matchValue
-                && matchWordIndex == o.matchWordIndex);
-    }
-};
-
-struct CacheEntry
-{
-    u32 extractMask = 0;
-    u8 extractBits  = 0;
-#ifndef A2_DATA_FILTER_ALWAYS_GATHER
-    bool needGather = false;
-    u8 extractShift = 0;
-#endif
-};
-
-DataFilter make_filter(const std::string &filter, s32 wordIndex = -1);
-
-inline bool matches(const DataFilter &filter, u32 value, s32 wordIndex = -1)
-{
-    return ((filter.matchWordIndex < 0) || (filter.matchWordIndex == wordIndex))
-        && ((value & filter.matchMask) == filter.matchValue);
-}
-
-CacheEntry make_cache_entry(const DataFilter &filter, char marker);
-
-// Note: a match is assumed.
-inline u32 extract(const CacheEntry &cache, u32 value)
-{
-#ifdef A2_DATA_FILTER_ALWAYS_GATHER
-    u32 result = bit_gather(value, cache.extractMask);
-#else
-    u32 result = ((value & cache.extractMask) >> cache.extractShift);
-
-    if (cache.needGather)
-    {
-        result = bit_gather(result, cache.extractMask >> cache.extractShift);
-    }
-#endif
-    return result;
-}
-
-// Note: a match is assumed.
-inline u32 extract(const DataFilter &filter, u32 value, char marker)
-{
-    auto cache = make_cache_entry(filter, marker);
-    return extract(cache, value);
-}
-
-inline u8 get_extract_bits(const DataFilter &filter, char marker)
-{
-    return make_cache_entry(filter, marker).extractBits;
-}
-
-inline u32 get_extract_mask(const DataFilter &filter, char marker)
-{
-    return make_cache_entry(filter, marker).extractMask;
-}
-
-inline u8 get_extract_shift(const DataFilter &filter, char marker)
-{
-    return make_cache_entry(filter, marker).extractShift;
-}
-
-std::string to_string(const DataFilter &filter);
+using mesytec::mvlc::util::FilterSize;
+using mesytec::mvlc::util::DataFilter;
+using mesytec::mvlc::util::CacheEntry;
+using mesytec::mvlc::util::make_filter;
+using mesytec::mvlc::util::matches;
+using mesytec::mvlc::util::make_cache_entry;
+using mesytec::mvlc::util::extract;
+using mesytec::mvlc::util::get_extract_bits;
+using mesytec::mvlc::util::get_extract_mask;
+using mesytec::mvlc::util::get_extract_shift;
+using mesytec::mvlc::util::to_string;
 
 } // namespace data_filter
 } // namespace a2
