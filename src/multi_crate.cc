6eb633194979027f5e8ab4b50cc86a7c764a6dcf
Florian Lüke <f.lueke@mesytec.com>
2022-01-24 17:15:26 +0100

    continue work on multicrate readouts

--- src/multi_crate.cc
+++ src/multi_crate.cc
@@ -1,6 +1,9 @@
 #include "multi_crate.h"
 
 #include <cassert>
+#include <stdexcept>
+#include <fmt/format.h>
+#include <mesytec-mvlc/mesytec-mvlc.h>
 
 namespace multi_crate
 {
@@ -48,7 +51,7 @@ std::pair<std::unique_ptr<VMEConfig>, MultiCrateModuleMappings> make_merged_vme_
                     for (auto moduleConf: moduleConfigs)
                     {
                         auto moduleCopy = copy_config_object(moduleConf);
-                        insert_module_mapping(mappings, moduleConf, moduleCopy.get());
+                        mappings.insertMapping(moduleConf, moduleCopy.get());
                         outEv->addModuleConfig(moduleCopy.release());
                     }
                 }
@@ -82,7 +85,7 @@ std::pair<std::unique_ptr<VMEConfig>, MultiCrateModuleMappings> make_merged_vme_
                 for (auto moduleConf: moduleConfigs)
                 {
                     auto moduleCopy = copy_config_object(moduleConf);
-                    insert_module_mapping(mappings, moduleConf, moduleCopy.get());
+                    mappings.insertMapping(moduleConf, moduleCopy.get());
                     outEv->addModuleConfig(moduleCopy.release());
                 }
 
@@ -100,6 +103,171 @@ std::pair<std::unique_ptr<VMEConfig>, MultiCrateModuleMappings> make_merged_vme_
         merged->addEventConfig(eventConf.release());
 
     return std::make_pair(std::move(merged), mappings);
+}
+
+QJsonObject to_json_object(const MultiCrateConfig &mcfg)
+{
+    QJsonObject j;
+    j["mainConfig"] = mcfg.mainConfig;
+    j["secondaryConfigs"] = QJsonArray::fromStringList(mcfg.secondaryConfigs);
+
+    QJsonArray ids;
+
+    std::transform(
+        std::begin(mcfg.crossCrateEventIds), std::end(mcfg.crossCrateEventIds),
+        std::back_inserter(ids), [] (const QUuid &id)
+        {
+            return id.toString();
+        });
+
+    j["crossCrateEventIds"] = ids;
+
+    return j;
+}
+
+QJsonDocument to_json_document(const MultiCrateConfig &mcfg)
+{
+    QJsonObject outer;
+    outer["MvmeMultiCrateConfig"] = to_json_object(mcfg);
+    return QJsonDocument(outer);
+}
+
+MultiCrateConfig load_multi_crate_config(const QJsonObject &json)
+{
+    MultiCrateConfig mcfg = {};
+    mcfg.mainConfig = json["mainConfig"].toString();
+
+    for (const auto &jval: json["secondaryConfigs"].toArray())
+        mcfg.secondaryConfigs.push_back(jval.toString());
+
+    for (const auto &jval: json["crossCrateEventIds"].toArray())
+        mcfg.crossCrateEventIds.insert(QUuid::fromString(jval.toString()));
+
+    return mcfg;
+}
+
+MultiCrateConfig load_multi_crate_config(const QJsonDocument &doc)
+{
+    return load_multi_crate_config(doc.object()["MvmeMultiCrateConfig"].toObject());
+}
+
+MultiCrateConfig load_multi_crate_config(const QString &filename)
+{
+    QFile inFile(filename);
+
+    if (!inFile.open(QIODevice::ReadOnly))
+        throw std::runtime_error(inFile.errorString().toStdString());
+
+    auto data = inFile.readAll();
+
+    QJsonParseError parseError;
+    QJsonDocument doc(QJsonDocument::fromJson(data, &parseError));
+
+    if (parseError.error != QJsonParseError::NoError)
+    {
+        throw std::runtime_error(
+            fmt::format("JSON parse error in file {}: {} at offset {}",
+                        filename.toStdString(),
+                        parseError.errorString().toStdString(),
+                        std::to_string(parseError.offset)));
+    }
+
+    if (doc.isNull())
+        return {};
+
+    return load_multi_crate_config(doc);
+}
+
+//
+// Playground
+//
+
+using namespace mesytec;
+
+// Chain to build:
+// mvlc::ReadoutWorker -> listfile::WriteHandle
+//   -> [write  single crate listfile to disk]
+//   -> Queue -> ReadoutParser -> callback -> EventBuilder::recordEventData()
+//   -> EventBuilder -> MergedQueue
+//   -> MergedQueue -> Analysis
+//                  -> MergedListfile
+//
+// Can leave out the merged queue in the fist iteration and directly call into
+// the analysis but this makes EventBuilder and analysis run in the same
+// thread.
+
+// Threads, ReadoutWorker, ReadoutParser, EventBuilder, Analysis
+
+
+
+struct CrateReadout
+{
+    mvlc::MVLC mvlc;
+    mvlc::ReadoutBufferQueues readoutSnoopQueues; // unused but required for mvlc::ReadoutWorker
+    std::unique_ptr<mvlc::ReadoutWorker> readoutWorker;
+
+    // Buffer queues between the readout worker and the readout parser. Filled
+    // by an instance of BufferQueuesWriteHandle, emptied by the readout
+    // parser.
+    mvlc::ReadoutBufferQueues listfileBufferQueues;
+
+    mvlc::readout_parser::ReadoutParserState parserState;
+    mvlc::Protected<mvlc::readout_parser::ReadoutParserCounters> parserCounters;
+    mvlc::readout_parser::ReadoutParserCallbacks parserCallbacks;
+    std::atomic<bool> parserQuit;
+    std::thread parserThread;
+};
+
+struct MultiCrateReadout
+{
+    std::vector<CrateReadout> crateReadouts;
+
+    mvlc::EventBuilder eventBuilder;
+    mvlc::readout_parser::ReadoutParserCallbacks eventBuilderCallbacks;
+    std::thread eventBuilderThread; // XXX: leftoff here
+};
+
+// listfile::WriteHandle implementation which enqueues the data onto the given
+// ReadoutBufferQueues. The queues are used in a blocking fashion which means
+// write() will block if the consuming side is too slow.
+class BufferQueuesWriteHandle: public mvlc::listfile::WriteHandle
+{
+    public:
+        BufferQueuesWriteHandle(
+            mvlc::ReadoutBufferQueues &destQueues,
+            mvlc::ConnectionType connectionType
+            )
+            : destQueues_(destQueues)
+            , connectionType_(connectionType)
+        {
+        }
+
+        size_t write(const u8 *data, size_t size) override
+        {
+            auto destBuffer = destQueues_.emptyBufferQueue().dequeue_blocking();
+            destBuffer->clear();
+            destBuffer->setBufferNumber(nextBufferNumber_++);
+            destBuffer->setType(connectionType_);
+            destBuffer->ensureFreeSpace(size);
+            assert(destBuffer->used() == 0);
+            std::memcpy(destBuffer->data(), data, size);
+            destBuffer->use(size);
+            destQueues_.filledBufferQueue().enqueue(destBuffer);
+            return size;
+        }
+
+    private:
+        mvlc::ReadoutBufferQueues &destQueues_;
+        mvlc::ConnectionType connectionType_;
+        u32 nextBufferNumber_ = 1u;
+};
+
+// run_readout_parser()
+// run_event_builder()
+
+void multi_crate_playground()
+{
+    CrateReadout crateReadout;
 }
 
 }
