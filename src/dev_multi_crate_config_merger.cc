05603d7cfcee850d9e91771b71e4ac7ce02c656d
Florian Lüke <f.lueke@mesytec.com>
2022-02-17 17:08:01 +0100

    more work on multi_crate things
    Note: libmvme now publicly links against Lyra which comes with mesytec-mvlc

--- src/dev_multi_crate_config_merger.cc
+++ src/dev_multi_crate_config_merger.cc
@@ -1,27 +1,71 @@
 #include <QApplication>
 #include <iostream>
 #include <fmt/format.h>
+#include <lyra/lyra.hpp>
+#include <set>
+#include <vector>
 
 #include "mvme_session.h"
 #include "multi_crate.h"
 #include "vme_config.h"
+#include "vme_config_tree.h"
 #include "vme_config_util.h"
+
+using std::cerr;
+using std::cout;
+using std::endl;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     mvme_init("dev_multi_crate_config_merger");
 
-    auto args = app.arguments();
+    bool opt_showHelp = false;
+    std::vector<int> opt_crossCrateEvents;
+    std::vector<std::string> opt_crateConfigPaths;
 
-    if (args.size() < 3)
+    auto cli
+        = lyra::help(opt_showHelp)
+
+        | lyra::opt(opt_crossCrateEvents, "event index")
+            .name("-e")
+            ["--cross-event"]("zero based event indexes specifying cross crate events")
+            .cardinality(0, 10)
+
+        | lyra::arg(opt_crateConfigPaths, "crate vme config")
+            .cardinality(1, 10)
+        ;
+
+    auto cliParseResult = cli.parse({ argc, argv });
+
+    if (!cliParseResult)
+    {
+        cerr << "Error parsing command line arguments: "
+            << cliParseResult.errorMessage() << endl;
         return 1;
+    }
 
-    args.pop_front(); // pop off the exe name
+    if (opt_showHelp)
+    {
+        cout << cli << endl;
+
+        cout << "write me!" << endl;
+        return 0;
+    }
+
+    QStringList crateConfigPaths;
+
+    for (auto path: opt_crateConfigPaths)
+        crateConfigPaths.append(QString::fromStdString(path));
+
+    std::set<int> crossCrateEvents;
+
+    for (auto ei: opt_crossCrateEvents)
+        crossCrateEvents.insert(ei);
 
     std::vector<std::unique_ptr<VMEConfig>> crateConfigs;
 
-    for (auto filename: args)
+    for (auto filename: crateConfigPaths)
     {
         auto readResult = read_vme_config_from_file(filename);
         if (!readResult.second.isEmpty())
@@ -34,18 +78,16 @@ int main(int argc, char *argv[])
         crateConfigs.emplace_back(std::move(readResult.first));
     }
 
-    std::vector<VMEConfig *> rawCrateConfigs;
+    auto mergeResult = mesytec::multi_crate::make_merged_vme_config(crateConfigs, crossCrateEvents);
 
-    for (auto &crateConf: crateConfigs)
-        rawCrateConfigs.push_back(crateConf.get());
+    //auto jsonDoc = mvme::vme_config::serialize_vme_config_to_json_document(*mergeResult.first);
+    //auto jsonText = jsonDoc.toJson();
+    //std::cout << jsonText.toStdString();
 
-    std::set<int> crossCrateEvents = { 0 };
+    VMEConfigTreeWidget configTreeWidget;
+    configTreeWidget.resize(600, 800);
+    configTreeWidget.setConfig(mergeResult.first.get());
+    configTreeWidget.show();
 
-    auto mergeResult = multi_crate::make_merged_vme_config(rawCrateConfigs, crossCrateEvents);
-
-    auto jsonDoc = mvme::vme_config::serialize_vme_config_to_json_document(*mergeResult.first);
-    auto jsonText = jsonDoc.toJson();
-    std::cout << jsonText.toStdString();
-
-    return 0;
+    return app.exec();
 }
