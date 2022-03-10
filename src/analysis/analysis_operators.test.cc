4bebddc9d73aef882968d6f7e14a2fba99f7863e
Florian Lüke <f.lueke@mesytec.com>
2022-03-10 16:37:26 +0100

    add MultiHitExtractorTest

---
+++ src/analysis/analysis_operators.test.cc
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "analysis.h"
+
+TEST(MultiHitExtractorTest, ArrayPerHitShape)
+{
+    using namespace analysis;
+
+    MultiHitExtractor mex;
+    // 4 address bits -> 16 addresses
+    // 5 data bits -> data upper bound is 32
+    // maximum of 3 hits per address
+    mex.setObjectName("channel_time");
+    mex.setFilter(make_filter("AAAADDDDD"));
+    mex.setMaxHits(3);
+    mex.setShape(MultiHitExtractor::Shape::ArrayPerHit);
+
+    mex.beginRun({});
+
+    ASSERT_EQ(mex.getNumberOfOutputs(), 3);
+    ASSERT_EQ(mex.getOutputName(0), "channel_time_hit0");
+    ASSERT_EQ(mex.getOutputName(1), "channel_time_hit1");
+    ASSERT_EQ(mex.getOutputName(2), "channel_time_hit2");
+
+    for (int i=0; i<mex.getNumberOfOutputs(); ++i)
+    {
+        auto output = mex.getOutput(i);
+        ASSERT_EQ(output->getSize(), 16);
+
+        for (const auto &param: output->getParameters())
+        {
+            ASSERT_EQ(param.lowerLimit, 0.0);
+            ASSERT_EQ(param.upperLimit, 32.0);
+        }
+    }
+
+    {
+        auto clone = mex.clone();
+        auto mex2 = qobject_cast<MultiHitExtractor *>(clone.get());
+        ASSERT_EQ(mex.getFilter(), mex2->getFilter());
+        ASSERT_EQ(mex.getMaxHits(), mex2->getMaxHits());
+        ASSERT_EQ(mex.getShape(), mex2->getShape());
+        ASSERT_EQ(mex.getOptions(), mex2->getOptions());
+    }
+}
+
+TEST(MultiHitExtractorTest, ArrayPerAddress)
+{
+    using namespace analysis;
+
+    MultiHitExtractor mex;
+    // 4 address bits -> 16 addresses
+    // 5 data bits -> data upper bound is 32
+    // maximum of 3 hits per address
+    mex.setObjectName("channel_time");
+    mex.setFilter(make_filter("AAAADDDDD"));
+    mex.setMaxHits(3);
+    mex.setShape(MultiHitExtractor::Shape::ArrayPerAddress);
+
+    mex.beginRun({});
+
+    ASSERT_EQ(mex.getNumberOfOutputs(), 16);
+
+    for (int i=0; i<mex.getNumberOfOutputs(); ++i)
+    {
+        auto output = mex.getOutput(i);
+
+        ASSERT_EQ(output->getSize(), 3);
+
+        for (const auto &param: output->getParameters())
+        {
+            ASSERT_EQ(param.lowerLimit, 0.0);
+            ASSERT_EQ(param.upperLimit, 32.0);
+        }
+
+        ASSERT_EQ(mex.getOutputName(i), QSL("channel_time%1_hits").arg(i));
+    }
+
+    {
+        auto clone = mex.clone();
+        auto mex2 = qobject_cast<MultiHitExtractor *>(clone.get());
+        ASSERT_EQ(mex.getFilter(), mex2->getFilter());
+        ASSERT_EQ(mex.getMaxHits(), mex2->getMaxHits());
+        ASSERT_EQ(mex.getShape(), mex2->getShape());
+        ASSERT_EQ(mex.getOptions(), mex2->getOptions());
+    }
+}
