53f90fd3bb91ab6482ab97082fb913f1b57ca84c
Florian Lüke <f.lueke@mesytec.com>
2022-04-08 15:27:49 +0200

    [analysis] disable special handling of connections to arrays of size 1
    The special case turned connections to arrays of size 1 to indexed
    connections with index=0. This is now disabled because it caused problems
    when trying to find an appropriate plot to display a condition in.

--- src/analysis/analysis.cc
+++ src/analysis/analysis.cc
@@ -275,6 +275,7 @@ void Slot::connectPipe(Pipe *newInput, s32 newParamIndex)
 
     if (newInput)
     {
+#if 0
         // Note(20201209): This was added in 2018 to make handling of arrays
         // of size 1 "more intuitive".
 
@@ -282,10 +283,14 @@ void Slot::connectPipe(Pipe *newInput, s32 newParamIndex)
         // "value" connection using index 0.
         // Basically NoParamIndex is rewritten to 0 in case the input is of size 1
         // and it's ok to use Input::Value.
+
+        // Update (220408): Disabled this special case as it interferes with
+        // condition editing:
         if (newInput->getSize() == 1 && acceptedInputTypes & InputType::Value)
         {
             newParamIndex = 0;
         }
+#endif
 
         inputPipe = newInput;
         paramIndex = newParamIndex;
@@ -417,6 +422,14 @@ void SinkInterface::postClone(const AnalysisObject *cloneSource)
     assert(si);
     this->setEnabled(si->isEnabled());
     OperatorInterface::postClone(cloneSource);
+}
+
+//
+// ConditionInterface
+//
+ConditionInterface::~ConditionInterface()
+{
+    qDebug() << __PRETTY_FUNCTION__ << this;
 }
 
 //
