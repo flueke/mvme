4b0d3f53725a2738e9e66f1809630f04f8c08a09
Florian Lüke <f.lueke@mesytec.com>
2023-10-25 20:03:05 +0200

    prometheus: refactor code so things work on context creation error

--- src/mvme_prometheus.cc
+++ src/mvme_prometheus.cc
@@ -30,98 +30,116 @@ PrometheusContext::PrometheusContext(const std::string &bind_address)
 
 struct StreamProcCountersPromExporter::Private
 {
-    Logger logger_;
-    prometheus::Family<prometheus::Gauge> &bytes_processed_family_;
-    prometheus::Gauge &bytes_processed_;
-
-    prometheus::Family<prometheus::Gauge> &buffers_processed_family_;
-    prometheus::Gauge &buffers_processed_;
-
-    prometheus::Family<prometheus::Gauge> &events_processed_family_;
-    prometheus::Gauge &events_processed_;
-
-    prometheus::Family<prometheus::Gauge> &event_hits_family_;
-    std::array<prometheus::Gauge *, MaxVMEEvents> event_hits_;
-
-    using PrometheusModuleHits = std::array<prometheus::Gauge *, MaxVMEModules>;
-    prometheus::Family<prometheus::Gauge> &module_hits_family_;
-    std::array<PrometheusModuleHits, MaxVMEEvents> module_hits_;
-
-    Private(prometheus::Registry &registry)
-        : bytes_processed_family_(prometheus::BuildGauge()
-                                      .Name("bytes_processed")
-                                      .Help("Bytes processed by the mvme analysis")
-                                      .Register(registry))
-        , bytes_processed_(bytes_processed_family_.Add({}))
-
-        , buffers_processed_family_(prometheus::BuildGauge()
-                                      .Name("buffers_processed")
-                                      .Help("Buffers processed by the mvme analysis")
-                                      .Register(registry))
-        , buffers_processed_(buffers_processed_family_.Add({}))
-
-        , events_processed_family_(prometheus::BuildGauge()
-                                      .Name("events_processed")
-                                      .Help("Total events processed by the mvme analysis")
-                                      .Register(registry))
-        , events_processed_(events_processed_family_.Add({}))
-
-        , event_hits_family_(prometheus::BuildGauge()
-                                      .Name("events_hits")
-                                      .Help("Events processed by the mvme analysis")
-                                      .Register(registry))
-
-        , module_hits_family_(prometheus::BuildGauge()
-                                      .Name("module_hits")
-                                      .Help("Per module processed events by the mvme analysis")
-                                      .Register(registry))
+    struct Metrics
     {
-        std::fill(std::begin(event_hits_), std::end(event_hits_), nullptr);
+        prometheus::Family<prometheus::Gauge> &bytes_processed_family_;
+        prometheus::Gauge &bytes_processed_;
 
-        for (size_t i=0; i<module_hits_.size(); ++i)
-            std::fill(std::begin(module_hits_[i]), std::end(module_hits_[i]), nullptr);
+        prometheus::Family<prometheus::Gauge> &buffers_processed_family_;
+        prometheus::Gauge &buffers_processed_;
+
+        prometheus::Family<prometheus::Gauge> &events_processed_family_;
+        prometheus::Gauge &events_processed_;
+
+        prometheus::Family<prometheus::Gauge> &event_hits_family_;
+        std::array<prometheus::Gauge *, MaxVMEEvents> event_hits_;
+
+        using PrometheusModuleHits = std::array<prometheus::Gauge *, MaxVMEModules>;
+        prometheus::Family<prometheus::Gauge> &module_hits_family_;
+        std::array<PrometheusModuleHits, MaxVMEEvents> module_hits_;
+
+        Metrics(prometheus::Registry &registry)
+            : bytes_processed_family_(prometheus::BuildGauge()
+                                        .Name("bytes_processed")
+                                        .Help("Bytes processed by the mvme analysis")
+                                        .Register(registry))
+            , bytes_processed_(bytes_processed_family_.Add({}))
+
+            , buffers_processed_family_(prometheus::BuildGauge()
+                                        .Name("buffers_processed")
+                                        .Help("Buffers processed by the mvme analysis")
+                                        .Register(registry))
+            , buffers_processed_(buffers_processed_family_.Add({}))
+
+            , events_processed_family_(prometheus::BuildGauge()
+                                        .Name("events_processed")
+                                        .Help("Total events processed by the mvme analysis")
+                                        .Register(registry))
+            , events_processed_(events_processed_family_.Add({}))
+
+            , event_hits_family_(prometheus::BuildGauge()
+                                        .Name("events_hits")
+                                        .Help("Events processed by the mvme analysis")
+                                        .Register(registry))
+
+            , module_hits_family_(prometheus::BuildGauge()
+                                        .Name("module_hits")
+                                        .Help("Per module processed events by the mvme analysis")
+                                        .Register(registry))
+        {
+            std::fill(std::begin(event_hits_), std::end(event_hits_), nullptr);
+
+            for (size_t i=0; i<module_hits_.size(); ++i)
+                std::fill(std::begin(module_hits_[i]), std::end(module_hits_[i]), nullptr);
+        }
+    };
+
+    Logger logger_;
+    std::unique_ptr<Metrics> metrics_;
+
+    Private()
+    {
+        if (auto prom = get_prometheus_instance())
+        {
+            if (auto registry = prom->registry())
+            {
+                metrics_ = std::make_unique<Metrics>(*registry);
+            }
+        }
     }
 
     void recreateMetrics(const VMEConfig *vmeConfig)
     {
-        for (size_t i=0; i<event_hits_.size(); ++i)
+        if (!metrics_) return;
+
+        for (size_t i=0; i<metrics_->event_hits_.size(); ++i)
         {
-            if (event_hits_[i])
+            if (metrics_->event_hits_[i])
             {
-                event_hits_family_.Remove(event_hits_[i]);
-                event_hits_[i] = nullptr;
+                metrics_->event_hits_family_.Remove(metrics_->event_hits_[i]);
+                metrics_->event_hits_[i] = nullptr;
             }
         }
 
-        for (size_t i=0; i<module_hits_.size(); ++i)
+        for (size_t i=0; i<metrics_->module_hits_.size(); ++i)
         {
-            for (size_t j=0; j<module_hits_[i].size(); ++j)
+            for (size_t j=0; j<metrics_->module_hits_[i].size(); ++j)
             {
-                if (module_hits_[i][j])
+                if (metrics_->module_hits_[i][j])
                 {
-                    module_hits_family_.Remove(module_hits_[i][j]);
-                    module_hits_[i][j] = nullptr;
+                    metrics_->module_hits_family_.Remove(metrics_->module_hits_[i][j]);
+                    metrics_->module_hits_[i][j] = nullptr;
                 }
             }
         }
 
         auto eventConfigs = vmeConfig->getEventConfigs();
 
-        for (size_t i=0; i<std::min(event_hits_.size(), static_cast<size_t>(eventConfigs.size())); ++i)
+        for (size_t i=0; i<std::min(metrics_->event_hits_.size(), static_cast<size_t>(eventConfigs.size())); ++i)
         {
-            event_hits_[i] = &event_hits_family_.Add({
+            metrics_->event_hits_[i] = &metrics_->event_hits_family_.Add({
                 {"event_index", std::to_string(i)},
                 {"event_name", eventConfigs[i]->objectName().toStdString()}
                 });
         }
 
-        for (size_t i=0; i<std::min(module_hits_.size(), static_cast<size_t>(eventConfigs.size())); ++i)
+        for (size_t i=0; i<std::min(metrics_->module_hits_.size(), static_cast<size_t>(eventConfigs.size())); ++i)
         {
             auto moduleConfigs = eventConfigs[i]->getModuleConfigs();
 
-            for (size_t j=0; j<std::min(module_hits_[i].size(), static_cast<size_t>(moduleConfigs.size())); ++j)
+            for (size_t j=0; j<std::min(metrics_->module_hits_[i].size(), static_cast<size_t>(moduleConfigs.size())); ++j)
             {
-                module_hits_[i][j] = &module_hits_family_.Add({
+                metrics_->module_hits_[i][j] = &metrics_->module_hits_family_.Add({
                     {"event_index", std::to_string(i)},
                     {"event_name", eventConfigs[i]->objectName().toStdString()},
                     {"module_index", std::to_string(j)},
@@ -133,29 +151,31 @@ struct StreamProcCountersPromExporter::Private
 
     void update(const MVMEStreamProcessorCounters &counters)
     {
-        bytes_processed_.Set(counters.bytesProcessed);
-        buffers_processed_.Set(counters.buffersProcessed);
-        events_processed_.Set(counters.totalEvents);
+        if (!metrics_) return;
 
-        for (size_t i=0; i<event_hits_.size(); ++i)
+        metrics_->bytes_processed_.Set(counters.bytesProcessed);
+        metrics_->buffers_processed_.Set(counters.buffersProcessed);
+        metrics_->events_processed_.Set(counters.totalEvents);
+
+        for (size_t i=0; i<metrics_->event_hits_.size(); ++i)
         {
-            if (event_hits_[i])
-                event_hits_[i]->Set(counters.eventCounters[i]);
+            if (metrics_->event_hits_[i])
+                metrics_->event_hits_[i]->Set(counters.eventCounters[i]);
         }
 
-        for (size_t i=0; i<event_hits_.size(); ++i)
+        for (size_t i=0; i<metrics_->event_hits_.size(); ++i)
         {
-            for (size_t j=0; j<event_hits_.size(); ++j)
+            for (size_t j=0; j<metrics_->event_hits_.size(); ++j)
             {
-                if (module_hits_[i][j])
-                    module_hits_[i][j]->Set(counters.moduleCounters[i][j]);
+                if (metrics_->module_hits_[i][j])
+                    metrics_->module_hits_[i][j]->Set(counters.moduleCounters[i][j]);
             }
         }
     }
 };
 
 StreamProcCountersPromExporter::StreamProcCountersPromExporter()
-    : d(std::make_unique<Private>(*get_prometheus_instance()->registry()))
+    : d(std::make_unique<Private>())
 {
 }
 
