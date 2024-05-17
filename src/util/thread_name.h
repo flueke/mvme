3fec4b058fd6921f50e045848bb23fe17add2d5f
Florian Lüke <f.lueke@mesytec.com>
2024-05-17 13:11:03 +0200

    add windows dll export

--- src/util/thread_name.h
+++ src/util/thread_name.h
@@ -1,9 +1,11 @@
 #ifndef CAF8B988_F0C9_475A_8E38_8789949859DB
 #define CAF8B988_F0C9_475A_8E38_8789949859DB
 
+#include "libmvme_export.h"
+
 namespace mesytec::util
 {
-void set_thread_name(const char *name);
+void LIBMVME_EXPORT set_thread_name(const char *name);
 }
 
 #endif /* CAF8B988_F0C9_475A_8E38_8789949859DB */
