ba0134376f24ec8e8395b9bba6dc2f198d68a1ad
Florian Lüke <f.lueke@mesytec.com>
2024-11-12 16:29:41 +0100

    a2: add more binning test, finally figure out the bin calculation issues
    TODO: limit user facing double input to +=1e15
    TODO: fix the tests and fix get_bin() and get_bin_unchecked() to make them
          work even if the numbers are larger/smaller than +=1e15

--- src/analysis/a2/test_a2.cc
+++ src/analysis/a2/test_a2.cc
@@ -2,7 +2,7 @@
 
 #include "a2.h"
 
-TEST(A2, histo_binning_1_to_1)
+TEST(A2, histo_binning_1_to_1_pos_only)
 {
     a2::Binning binning =
     {
@@ -24,13 +24,180 @@ TEST(A2, histo_binning_1_to_1)
     }
 
     {
-        double x = -1.0;
+        double x = binning.min - 1.0;
         s32 theBin = get_bin(binning, BinCount, x);
         ASSERT_EQ(theBin, a2::Binning::Underflow);
     }
 
     {
-        double x = 1024.0;
+        double x = binning.min + binning.range;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Overflow);
+    }
+}
+
+TEST(A2, histo_binning_1_to_1_neg_to_pos)
+{
+    a2::Binning binning =
+    {
+        .min = -512.0,
+        .range = 1024.0,
+    };
+
+    const size_t BinCount = 1024;
+    // This is from a2_adapter histo1d_sink_magic().
+    const double BinningFactor =  BinCount / binning.range;
+
+    for (size_t i=0; i<1024; ++i)
+    {
+        double x = binning.min + 1.0 * i;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, i);
+        //spdlog::info("x={}, theBin={}, binningFactor={}, binning={{.min={}, .range={}}}",
+        //    x, theBin, BinningFactor, binning.min, binning.range);
+    }
+
+    {
+        double x = binning.min - 1.0;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Underflow);
+    }
+
+    {
+        double x = binning.min + binning.range;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Overflow);
+    }
+}
+
+TEST(A2, histo_binning_less_bins)
+{
+    a2::Binning binning =
+    {
+        .min = -512.0,
+        .range = 1024.0,
+    };
+
+    const size_t BinCount = 512;
+    // This is from a2_adapter histo1d_sink_magic().
+    const double BinningFactor =  BinCount / binning.range;
+
+    size_t expectedBin = 0;
+
+    for (size_t i=0; i<1024; ++i)
+    {
+        double x = binning.min + 1.0 * i;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, expectedBin);
+        //spdlog::info("x={}, theBin={}, binningFactor={}, binning={{.min={}, .range={}}}",
+        //    x, theBin, BinningFactor, binning.min, binning.range);
+
+        if (i % 2 == 1)
+        {
+            ++expectedBin;
+        }
+    }
+
+    {
+        double x = binning.min - 1.0;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Underflow);
+    }
+
+    {
+        double x = binning.min + binning.range;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Overflow);
+    }
+}
+
+TEST(A2, histo_binning_more_bins)
+{
+    a2::Binning binning =
+    {
+        .min = -512.0,
+        .range = 1024.0,
+    };
+
+    const size_t BinCount = 2048;
+    // This is from a2_adapter histo1d_sink_magic().
+    const double BinningFactor =  BinCount / binning.range;
+
+    size_t expectedBin = 0;
+
+    for (size_t i=0; i<2048; ++i)
+    {
+        double x = binning.min + 0.5 * i;
+        s32 theBin = get_bin(binning, BinCount, x);
+        //spdlog::info("x={}, theBin={}, binningFactor={}, binning={{.min={}, .range={}}}",
+        //    x, theBin, BinningFactor, binning.min, binning.range);
+        ASSERT_EQ(theBin, i);
+    }
+
+    {
+        double x = binning.min - 1.0;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Underflow);
+    }
+
+    {
+        double x = binning.min + binning.range;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Overflow);
+    }
+}
+
+//TEST(A2, histo_binning_get_bin)
+//{
+//}
+
+TEST(A2, histo_binning_large_range)
+{
+    const double v1 = -1e+16;
+    const long double v2 = v1 - 1.0l;
+    const double v3 = v1 - 1.0l;
+
+    spdlog::info("v1={}, v2={}, delta={}", v1, v2, v1 - v2);
+    spdlog::info("v1={}, v3={}, delta={}", v1, v3, v1 - v3);
+
+    // TODO: this is basically the limit we can do with doubles in the analysis.
+    // TODO: limit spinboxes and values in general to +-1e15
+    ASSERT_EQ(v1, v2);
+
+
+    a2::Binning binning =
+    {
+        .min = -1e+16, // FIXME: works up to -1e+15 but -1e+16 fails!
+        .range = +1e+20,
+    };
+
+    const size_t BinCount = 1024;
+    // This is from a2_adapter histo1d_sink_magic().
+    const double BinningFactor =  BinCount / binning.range;
+
+    spdlog::info("binning={{.min={}, .range={}}} => binningFactor={}", binning.min, binning.range, BinningFactor);
+
+    #if 0
+    size_t expectedBin = 0;
+
+    for (size_t i=0; i<1024; ++i)
+    {
+        double x = binning.min + 0.5 * i;
+        s32 theBin = get_bin(binning, BinCount, x);
+        spdlog::info("x={}, theBin={}, binningFactor={}, binning={{.min={}, .range={}}}",
+            x, theBin, BinningFactor, binning.min, binning.range);
+        ASSERT_EQ(theBin, i);
+    }
+    #endif
+
+    {
+        double x = binning.min - 1.0;
+        s32 theBin = get_bin(binning, BinCount, x);
+        ASSERT_EQ(theBin, a2::Binning::Underflow);
+    }
+
+    {
+        double x = binning.min + binning.range;
         s32 theBin = get_bin(binning, BinCount, x);
         ASSERT_EQ(theBin, a2::Binning::Overflow);
     }
