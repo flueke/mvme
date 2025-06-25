908bd1a71feb36f7fe264aa5c5791545bbf2909d
Florian Lüke <f.lueke@mesytec.com>
2025-06-25 15:35:20 +0200

    more histo additions tests

--- test/histograms.test.cc
+++ test/histograms.test.cc
@@ -2,6 +2,20 @@
 #include <gtest/gtest.h>
 #include <spdlog/spdlog.h>
 
+void dump_histo(const Histo1D &histo)
+{
+    auto binning = histo.getAxisBinning(Qt::XAxis);
+    auto width  = binning.getBinWidth();
+    spdlog::info("Histo1D: {} bins, range [{}, {}], binWidth={}, title={}", binning.getBins(), binning.getMin(),
+                 binning.getMax(), width, histo.getTitle().toLocal8Bit().data());
+
+    for (auto bin = 0u; bin < binning.getBins(); ++bin)
+    {
+        spdlog::info("Bin {} ({}, {}]): {}", bin, binning.getBinLowEdge(bin),
+                     binning.getBinLowEdge(bin) + width, histo.getBinContent(bin));
+    }
+}
+
 TEST(Histograms, AxisBinningGetBin)
 {
     AxisBinning binning(10, 0.0, 100.0);
@@ -40,8 +54,9 @@ TEST(Histograms, Histo1DGetCountsInRange)
     histo.fill(2.0);
     histo.fill(3.0);
     histo.fill(3.0);
+    histo.fill(9.0);
 
-    // value 0 1 1 2 0 0 0 0 0 0
+    // value 0 1 1 2 0 0 0 0 0 1
     // start 0 1 2 3 4 5 6 7 8 9
     // bin   0 1 2 3 4 5 6 7 8 9
 
@@ -50,6 +65,7 @@ TEST(Histograms, Histo1DGetCountsInRange)
     ASSERT_DOUBLE_EQ(histo.getBinContent(2), 1.0);
     ASSERT_DOUBLE_EQ(histo.getBinContent(3), 2.0);
     ASSERT_DOUBLE_EQ(histo.getBinContent(4), 0.0);
+    ASSERT_DOUBLE_EQ(histo.getBinContent(9), 1.0);
 
     // range is one bin wide
     ASSERT_DOUBLE_EQ(histo.getCounts(0.0, 1.0), 0.0);
@@ -57,12 +73,15 @@ TEST(Histograms, Histo1DGetCountsInRange)
     ASSERT_DOUBLE_EQ(histo.getCounts(2.0, 3.0), 1.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(3.0, 4.0), 2.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(4.0, 5.0), 0.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(9.0, 10.0), 1.0);
 
     // range is smaller than a bin
     ASSERT_DOUBLE_EQ(histo.getCounts(0.2, 0.8), 0.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(1.2, 1.8), 0.6);
     ASSERT_DOUBLE_EQ(histo.getCounts(2.2, 2.8), 0.6);
     ASSERT_DOUBLE_EQ(histo.getCounts(3.2, 3.8), 1.2);
+    const double epsilon = 0.0000001;
+    ASSERT_NEAR(histo.getCounts(9.2, 9.8), 0.6, epsilon);
 
     // range crosses multiple bins
     ASSERT_DOUBLE_EQ(histo.getCounts(0.0, 2.0), 1.0);
@@ -70,57 +89,70 @@ TEST(Histograms, Histo1DGetCountsInRange)
     ASSERT_DOUBLE_EQ(histo.getCounts(2.0, 4.0), 3.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(3.0, 5.0), 2.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(4.0, 6.0), 0.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(6.0, 10.0), 1.0);
 
     // multiple bins with fractional parts
     ASSERT_DOUBLE_EQ(histo.getCounts(1.2, 2.8), 0.8 + 0.8);
     ASSERT_DOUBLE_EQ(histo.getCounts(1.2, 3.8), 0.8 + 1.0 + 1.6);
+
+    // range goes outside the histograms range
+    ASSERT_DOUBLE_EQ(histo.getCounts(-1.0, 2.0), 1.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(8.0, 12.0), 1.0);
 }
 
 TEST(Histograms, Histo1DNegativeAxisGetCountsInRange)
 {
     const double epsilon = 0.0000001;
     Histo1D histo(10, -10.0, 0.0);
+    histo.setTitle("Negative Axis Histo");
 
-    histo.fill(-9.0);
+    histo.fill(-10.0);
+    histo.fill(-9.0, 2.0);
     histo.fill(-8.0);
-    histo.fill(-7.0);
-    histo.fill(-7.0);
+    histo.fill(-7.0, 2.0);
+    histo.fill(-1.0);
+
+    dump_histo(histo);
 
     // value   0  1  1  2  0  0  0  0  0  0
     // start -10 -9 -8 -7 -6 -5 -4 -3 -2 -1
     // bin     0  1  2  3  4  5  6  7  8  9
 
-    ASSERT_DOUBLE_EQ(histo.getBinContent(0), 0.0);
-    ASSERT_DOUBLE_EQ(histo.getBinContent(1), 1.0);
+    ASSERT_DOUBLE_EQ(histo.getBinContent(0), 1.0);
+    ASSERT_DOUBLE_EQ(histo.getBinContent(1), 2.0);
     ASSERT_DOUBLE_EQ(histo.getBinContent(2), 1.0);
     ASSERT_DOUBLE_EQ(histo.getBinContent(3), 2.0);
     ASSERT_DOUBLE_EQ(histo.getBinContent(4), 0.0);
+    ASSERT_DOUBLE_EQ(histo.getBinContent(9), 1.0);
 
     // range is one bin wide
-    ASSERT_DOUBLE_EQ(histo.getCounts(-10.0, -9.0), 0.0);
-    ASSERT_DOUBLE_EQ(histo.getCounts(-9.0, -8.0), 1.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(-10.0, -9.0), 1.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(-9.0, -8.0), 2.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(-8.0, -7.0), 1.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(-7.0, -6.0), 2.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(-6.0, -5.0), 0.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(-1.0, -0.0), 1.0);
 
     // range is smaller than a bin
-    ASSERT_NEAR(histo.getCounts(-9.8, -9.2), 0.0, epsilon);
-    ASSERT_NEAR(histo.getCounts(-8.8, -8.2), 0.6, epsilon);
+    ASSERT_NEAR(histo.getCounts(-9.8, -9.2), 0.6, epsilon);
+    ASSERT_NEAR(histo.getCounts(-8.8, -8.2), 1.2, epsilon);
     ASSERT_NEAR(histo.getCounts(-7.8, -7.2), 0.6, epsilon);
     ASSERT_NEAR(histo.getCounts(-6.8, -6.2), 1.2, epsilon);
+    ASSERT_NEAR(histo.getCounts(-0.8, -0.2), 0.6, epsilon);
 
     // range crosses multiple bins
-    ASSERT_DOUBLE_EQ(histo.getCounts(-10.0, -8.0), 1.0);
-    ASSERT_DOUBLE_EQ(histo.getCounts(-9.0, -7.0), 2.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(-10.0, -8.0), 3.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(-9.0, -7.0), 3.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(-8.0, -6.0), 3.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(-7.0, -5.0), 2.0);
     ASSERT_DOUBLE_EQ(histo.getCounts(-6.0, -4.0), 0.0);
+    ASSERT_DOUBLE_EQ(histo.getCounts(-1.5, -0.0), 1.0);
 
     // multiple bins with fractional parts
-    ASSERT_NEAR(histo.getCounts(-8.8, -8.0), 0.8, epsilon);
-    ASSERT_NEAR(histo.getCounts(-8.8, -7.0), 0.8 + 1.0, epsilon);
-    ASSERT_NEAR(histo.getCounts(-8.8, -6.0), 0.8 + 1.0 + 2.0, epsilon);
-    ASSERT_NEAR(histo.getCounts(-8.8, -6.2), 0.8 + 1.0 + 0.8 * 2.0, epsilon);
+    ASSERT_NEAR(histo.getCounts(-8.8, -8.0), 1.6, epsilon);
+    ASSERT_NEAR(histo.getCounts(-8.8, -7.0), 1.6 + 1.0, epsilon);
+    ASSERT_NEAR(histo.getCounts(-8.8, -6.0), 1.6 + 1.0 + 2.0, epsilon);
+    ASSERT_NEAR(histo.getCounts(-8.8, -6.2), 1.6 + 1.0 + 0.8 * 2.0, epsilon);
 }
 
 TEST(Histograms, AddHistogramsSimple)
@@ -139,19 +171,6 @@ TEST(Histograms, AddHistogramsSimple)
     for (auto bin = 0u; bin < histo1.getNumberOfBins(); ++bin)
     {
         ASSERT_DOUBLE_EQ(result->getBinContent(bin), histo1.getBinContent(bin) * 2.0);
-    }
-}
-
-void dump_histo(const Histo1D &histo)
-{
-    auto binning = histo.getAxisBinning(Qt::XAxis);
-    spdlog::info("Histo1D: {} bins, range [{}, {}], title={}", binning.getBins(), binning.getMin(),
-                 binning.getMax(), histo.getTitle().toLocal8Bit().data());
-
-    for (auto bin = 0u; bin < binning.getBins(); ++bin)
-    {
-        spdlog::info("Bin {} ({}, {}]): {}", bin, binning.getBinLowEdge(bin),
-                     binning.getBinLowEdge(bin) + binning.getBinWidth(), histo.getBinContent(bin));
     }
 }
 
@@ -209,7 +228,6 @@ TEST(Histograms, AddHistogramsSameBinsDifferentRanges)
     }
 }
 
-#if 0
 TEST(Histograms, AddHistogramsUnequalBinningsMinBins)
 {
     Histo1D histo1(10, 0.0, 10.0);
@@ -237,10 +255,15 @@ TEST(Histograms, AddHistogramsUnequalBinningsMinBins)
     dump_histo(histo2);
     dump_histo(*result);
 
-    const double epsilon = 0.0000001;
+    const double epsilon = 0.001;
     ASSERT_NEAR(result->getCounts(0.0, 2.0), 1.0, epsilon);
     ASSERT_NEAR(result->getCounts(2.0, 4.0), 3.0, epsilon);
-    ASSERT_NEAR(result->getCounts(4.0, 6.0), 0.0, epsilon);
+    ASSERT_NEAR(result->getCounts(4.0, 6.0), 1.333, epsilon);
+    ASSERT_NEAR(result->getCounts(6.0, 8.0), 0.666, epsilon);
+    ASSERT_NEAR(result->getCounts(8.0, 10.0), 0.666, epsilon);
+    ASSERT_NEAR(result->getCounts(10.0, 12.0), 0.333, epsilon);
+    ASSERT_NEAR(result->getCounts(12.0, 18.0), 0.0, epsilon);
+    ASSERT_NEAR(result->getCounts(18.0, 20.0), 1.0, epsilon);
 }
 
 TEST(Histograms, AddHistogramsUnequalBinningsMaxBins)
@@ -264,7 +287,19 @@ TEST(Histograms, AddHistogramsUnequalBinningsMaxBins)
     ASSERT_EQ(binning.getBins(), 20u);
     ASSERT_EQ(binning.getMin(), 0.0);
     ASSERT_EQ(binning.getMax(), 20.0);
+    ASSERT_EQ(binning.getBinWidth(), 1.0);
 
+    dump_histo(histo1);
+    dump_histo(histo2);
     dump_histo(*result);
+
+    const double epsilon = 0.001;
+    ASSERT_NEAR(result->getCounts(0.0, 2.0), 1.0, epsilon);
+    ASSERT_NEAR(result->getCounts(2.0, 4.0), 3.0, epsilon);
+    ASSERT_NEAR(result->getCounts(4.0, 6.0), 1.333, epsilon);
+    ASSERT_NEAR(result->getCounts(6.0, 8.0), 0.666, epsilon);
+    ASSERT_NEAR(result->getCounts(8.0, 10.0), 0.666, epsilon);
+    ASSERT_NEAR(result->getCounts(10.0, 12.0), 0.333, epsilon);
+    ASSERT_NEAR(result->getCounts(12.0, 18.0), 0.0, epsilon);
+    ASSERT_NEAR(result->getCounts(18.0, 20.0), 1.0, epsilon);
 }
-#endif
