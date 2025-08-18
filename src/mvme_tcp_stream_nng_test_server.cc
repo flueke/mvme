2164d8464d6b109a0e701e64e9e195de80e207a3
Florian Lüke <f.lueke@mesytec.com>
2025-08-18 12:14:17 +0200

    nng_test_server: multi listener support and fix all leaks
    nng_fini() makes nng deallocate all internal state which makes valgrind
    happy. Don't call it in mvme though as nng would be unusable afterwards.

--- src/mvme_tcp_stream_nng_test_server.cc
+++ src/mvme_tcp_stream_nng_test_server.cc
@@ -47,78 +47,87 @@ struct ClientConnection
     std::string remoteAddressString() const { return nng::nng_sockaddr_to_string(remoteAddress()); }
 };
 
+struct ServerContext;
+
+struct Acceptor
+{
+    ServerContext *ctx = nullptr;
+    nng_stream_listener *listener = nullptr;
+    nng_aio *accept_aio = nullptr;
+};
+
 struct ServerContext
 {
-    nng_stream_listener *listener;
-    nng_aio *accept_aio;
+    std::vector<Acceptor> acceptors;
     std::vector<std::unique_ptr<ClientConnection>> clients;
     std::mutex clients_mutex;
     std::atomic<bool> shutdown{false};
 
     ServerContext()
-        : listener(nullptr)
-        , accept_aio(nullptr)
     {
     }
 
     ~ServerContext()
     {
-        if (accept_aio)
+        spdlog::info("~ServerContext: Shutting down server");
+        shutdown = true;
+
+        for (auto &acceptor: acceptors)
         {
-            nng_aio_free(accept_aio);
-        }
-        if (listener)
-        {
-            nng_stream_listener_free(listener);
+            if (acceptor.accept_aio)
+            {
+                nng_aio_cancel(acceptor.accept_aio);
+                nng_aio_wait(acceptor.accept_aio);
+            }
+            nng_aio_free(acceptor.accept_aio);
+            nng_stream_listener_free(acceptor.listener);
         }
     }
 };
 
 void accept_cb(void *arg);
 
-void start_accept(ServerContext *ctx)
+void start_accept(Acceptor *acceptor)
 {
-    if (ctx->shutdown)
+    if (acceptor->ctx->shutdown)
         return;
 
-    if (ctx->accept_aio == nullptr)
+    if (!acceptor->accept_aio)
     {
-        int rv = nng_aio_alloc(&ctx->accept_aio, accept_cb, ctx);
-        if (rv != 0)
+        if (int rv = nng_aio_alloc(&acceptor->accept_aio, accept_cb, acceptor))
         {
             spdlog::error("Failed to allocate accept AIO: {}", nng_strerror(rv));
             return;
         }
     }
 
-    nng_aio_set_timeout(ctx->accept_aio, 100);
-    nng_stream_listener_accept(ctx->listener, ctx->accept_aio);
+    nng_aio_set_timeout(acceptor->accept_aio, 100);
+    nng_stream_listener_accept(acceptor->listener, acceptor->accept_aio);
 }
 
 void accept_cb(void *arg)
 {
-    auto ctx = static_cast<ServerContext *>(arg);
+    auto acceptor = static_cast<Acceptor *>(arg);
 
-    if (ctx->shutdown)
+    if (acceptor->ctx->shutdown)
         return;
 
-    int rv = nng_aio_result(ctx->accept_aio);
-    if (rv != 0)
+    if (int rv = nng_aio_result(acceptor->accept_aio))
     {
         if (rv != NNG_ETIMEDOUT)
         {
             spdlog::error("Accept failed: {}, restarting", nng_strerror(rv));
         }
-        start_accept(ctx);
+        start_accept(acceptor);
         return;
     }
 
     // Retrieve the nng stream object from the aio
-    nng_stream *stream = static_cast<nng_stream *>(nng_aio_get_output(ctx->accept_aio, 0));
+    nng_stream *stream = static_cast<nng_stream *>(nng_aio_get_output(acceptor->accept_aio, 0));
     if (stream == nullptr)
     {
         spdlog::error("Accepted null stream");
-        start_accept(ctx);
+        start_accept(acceptor);
         return;
     }
 
@@ -129,8 +138,8 @@ void accept_cb(void *arg)
         // Add to client list
         {
             auto addrStr = client->remoteAddressString();
-            std::lock_guard<std::mutex> lock(ctx->clients_mutex);
-            ctx->clients.emplace_back(std::move(client));
+            std::lock_guard<std::mutex> lock(acceptor->ctx->clients_mutex);
+            acceptor->ctx->clients.emplace_back(std::move(client));
             spdlog::info("Accepted new connection from {}", addrStr);
         }
     } catch (const nng::exception &e)
@@ -139,7 +148,7 @@ void accept_cb(void *arg)
     }
 
     // Continue accepting
-    start_accept(ctx);
+    start_accept(acceptor);
 }
 
 // Send data to all clients in a blocking fashion
@@ -199,69 +208,95 @@ bool send_to_all_clients(ServerContext *ctx, u32 bufferNumber, const u32 *data, 
     return true;
 }
 
+const auto listenUris =
+{
+    "tcp4://*:42333",
+    "ipc:///tmp/mvme_tcp_stream_server.ipc",
+    "tcp4://*:42334",
+};
+
 int main()
 {
-    mvlc::set_global_log_level(spdlog::level::debug);
-    mvlc::util::setup_signal_handlers();
-
-    ServerContext ctx;
-
-    // Setup listener
-    int rv = nng_stream_listener_alloc(&ctx.listener, "tcp://0.0.0.0:42333");
-    if (rv != 0)
-    {
-        spdlog::error("Failed to allocate listener: {}", nng_strerror(rv));
-        return 1;
-    }
-
-    rv = nng_stream_listener_listen(ctx.listener);
-    if (rv != 0)
-    {
-        spdlog::error("Failed to listen: {}", nng_strerror(rv));
-        return 1;
-    }
-
-    // Display local address
-    nng_sockaddr local_addr{};
-    if (nng_stream_listener_get_addr(ctx.listener, NNG_OPT_LOCADDR, &local_addr) == 0)
-    {
-        spdlog::info("Listening on {}", nng::nng_sockaddr_to_string(local_addr));
-    }
-
-    // Start accepting connections
-    start_accept(&ctx);
-
-    // Main loop - send data periodically to all clients
-    u32 bufferNumber = 0;
-    std::vector<u32> data = {1, 2, 3, 4, 5};
-
-    while (!mvlc::util::signal_received())
-    {
-        // spdlog::info("Main loop iteration, {} clients connected. bufferNumber={}",
-        // ctx.clients.size(), bufferNumber);
-
-        if (!send_to_all_clients(&ctx, bufferNumber, data.data(), data.size()))
         {
-            spdlog::warn("Failed to send data to all clients");
+        mvlc::set_global_log_level(spdlog::level::debug);
+        mvlc::util::setup_signal_handlers();
+
+        ServerContext ctx;
+
+        // Start listening on all configures URIs
+        for (const auto &uri: listenUris)
+        {
+            Acceptor acceptor;
+            acceptor.ctx = &ctx;
+
+            if (int rv = nng_stream_listener_alloc(&acceptor.listener, uri))
+            {
+                spdlog::error("Failed to allocate listener for {}: {}", uri, nng_strerror(rv));
+                return 1;
+            }
+
+            if (int rv = nng_stream_listener_listen(acceptor.listener))
+            {
+                spdlog::error("Failed to listen on {}: {}", uri, nng_strerror(rv));
+                return 1;
+            }
+
+            nng_sockaddr local_addr{};
+            if (nng_stream_listener_get_addr(acceptor.listener, NNG_OPT_LOCADDR, &local_addr) == 0)
+            {
+                spdlog::info("Listening on {}", nng::nng_sockaddr_to_string(local_addr));
+            }
+
+            ctx.acceptors.emplace_back(std::move(acceptor));
         }
 
-        bufferNumber++;
-        std::for_each(std::begin(data), std::end(data), [](u32 &val) { val++; });
+        for (auto &acceptor: ctx.acceptors)
+        {
+            start_accept(&acceptor);
+        }
 
-        // simulate daq/replay delay here
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        // Main loop - send data periodically to all clients
+        u32 bufferNumber = 0;
+        std::vector<u32> data = {1, 2, 3, 4, 5};
+
+        while (!mvlc::util::signal_received())
+        {
+            // spdlog::info("Main loop iteration, {} clients connected. bufferNumber={}",
+            // ctx.clients.size(), bufferNumber);
+
+            if (!send_to_all_clients(&ctx, bufferNumber, data.data(), data.size()))
+            {
+                spdlog::warn("Failed to send data to all clients");
+            }
+
+            bufferNumber++;
+            std::for_each(std::begin(data), std::end(data), [](u32 &val) { val++; });
+
+            // simulate daq/replay delay here
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+
+        // Cleanup
+        spdlog::info("Shutting down server");
+        #if 0
+        ctx.shutdown = true;
+
+        for (auto &acceptor: ctx.acceptors)
+        {
+            if (acceptor.accept_aio)
+            {
+                nng_aio_cancel(acceptor.accept_aio);
+                nng_aio_wait(acceptor.accept_aio);
+            }
+            nng_stream_listener_free(acceptor.listener);
+            nng_aio_free(acceptor.accept_aio);
+        }
+        #endif
+
+
     }
 
-    // Cleanup
-    spdlog::info("Shutting down server");
-    ctx.shutdown = true;
-
-    if (ctx.accept_aio != nullptr)
-    {
-        // nng_aio_abort(ctx.accept_aio, NNG_ECANCELED);
-        nng_aio_cancel(ctx.accept_aio);
-        nng_aio_wait(ctx.accept_aio);
-    }
-
+    spdlog::info("left main scope. ServerContext should be destroyed by now");
+    nng_fini(); // Note: don't do this in mvme. It destroys state that NNG needs to operate. Helps valgrind a bit though.
     return 0;
 }
