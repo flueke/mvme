ec17d8cbb1d7d3f5f8d554095261dcfc86b45497
Florian Lüke <f.lueke@mesytec.com>
2025-10-15 13:19:57 +0200

    mdpp_sam: display timing info in the decoder monitor + clang-format

--- src/analysis/mdpp_sample_decoder_monitor_widget.cc
+++ src/analysis/mdpp_sample_decoder_monitor_widget.cc
@@ -1,12 +1,13 @@
 #include "mdpp_sample_decoder_monitor_widget.h"
 
-#include <QtConcurrent>
+#include <QStatusBar>
 #include <QTimer>
+#include <QtConcurrent>
 #include <mesytec-mvlc/util/stopwatch.h>
 
 #include "analysis/analysis.h"
-#include "util/qt_font.h"
 #include "ui_mdpp_sample_decoder_monitor_widget.h"
+#include "util/qt_font.h"
 
 using namespace mesytec::mvme;
 
@@ -22,9 +23,12 @@ struct DecoderDebugResult
     QString logText;
     QString inputText;
     QString outputText;
+
+    mesytec::mvlc::util::Stopwatch::duration_type dtDecode;
 };
 
-DecoderDebugResult run_decoder_collect_debug_info(const mesytec::mvme::mdpp_sampling::DecodedMdppSampleEvent &decodedByAnalysis)
+DecoderDebugResult run_decoder_collect_debug_info(
+    const mesytec::mvme::mdpp_sampling::DecodedMdppSampleEvent &decodedByAnalysis)
 {
     mesytec::mvlc::util::Stopwatch sw;
     DecoderDebugResult result;
@@ -32,7 +36,7 @@ DecoderDebugResult run_decoder_collect_debug_info(const mesytec::mvme::mdpp_samp
     QTextStream inputStream(&result.inputText);
     QTextStream outputStream(&result.outputText);
 
-    auto logger = [&] (const std::string &level, const std::string &message)
+    auto logger = [&](const std::string &level, const std::string &message)
     {
         logStream << fmt::format("[{}] {}\n", level, message).c_str();
 
@@ -51,18 +55,18 @@ DecoderDebugResult run_decoder_collect_debug_info(const mesytec::mvme::mdpp_samp
     }
 
     // Rerun the decoder, redirecting log output and checking for errors and warnings.
-    auto decoded = mdpp_sampling::decode_mdpp_samples(
-        inputData.data(), inputData.size(), decodedByAnalysis.moduleType.c_str(), logger);
+    auto decoded = mdpp_sampling::decode_mdpp_samples(inputData.data(), inputData.size(),
+                                                      decodedByAnalysis.moduleType.c_str(), logger);
 
-    for (size_t i=0; i<inputData.size(); ++i)
+    for (size_t i = 0; i < inputData.size(); ++i)
     {
         inputStream << fmt::format("{:3d}: {:#010x}\n", i, inputData[i]).c_str();
     }
 
     std::ostringstream oss;
     oss << fmt::format("module_type={}, input size={}\n", decoded.moduleType, inputData.size());
-    oss << fmt::format("module_header={:#010x}, moduleId={:04x}, timestamp={}\n",
-        decoded.header, decoded.headerModuleId, decoded.timestamp);
+    oss << fmt::format("module_header={:#010x}, moduleId={:04x}, timestamp={}\n", decoded.header,
+                       decoded.headerModuleId, decoded.timestamp);
     oss << fmt::format("#traces={}\n", decoded.traces.size());
 
     if (!decoded.traces.empty())
@@ -70,7 +74,7 @@ DecoderDebugResult run_decoder_collect_debug_info(const mesytec::mvme::mdpp_samp
         oss << "\n";
         const auto &traces = decoded.traces;
 
-        for (size_t traceIndex=0; traceIndex<static_cast<size_t>(traces.size()); ++traceIndex)
+        for (size_t traceIndex = 0; traceIndex < static_cast<size_t>(traces.size()); ++traceIndex)
         {
             auto thDebug = traces[traceIndex].traceHeader.parts.debug;
             auto thConfig = traces[traceIndex].traceHeader.parts.config;
@@ -79,25 +83,26 @@ DecoderDebugResult run_decoder_collect_debug_info(const mesytec::mvme::mdpp_samp
 
             oss << "trace #" << traceIndex << ":\n";
 
-            oss <<  fmt::format("  channel={}\n", traces[traceIndex].channel);
+            oss << fmt::format("  channel={}\n", traces[traceIndex].channel);
 
-            oss <<  fmt::format("  channel={}, #samples={}, trace header={:#010x}, .debug={}, .config={}, .phase={}, .length={}\n",
-                traces[traceIndex].channel, traces[traceIndex].samples.size(),
-                traces[traceIndex].traceHeader.value, thDebug, thConfig, thPhase, thLength);
+            oss << fmt::format("  channel={}, #samples={}, trace header={:#010x}, .debug={}, "
+                               ".config={}, .phase={}, .length={}\n",
+                               traces[traceIndex].channel, traces[traceIndex].samples.size(),
+                               traces[traceIndex].traceHeader.value, thDebug, thConfig, thPhase,
+                               thLength);
 
-            oss <<  fmt::format("  channel={}, samples: {}\n",
-                traces[traceIndex].channel, fmt::join(traces[traceIndex].samples, ", "));
+            oss << fmt::format("  channel={}, samples: {}\n", traces[traceIndex].channel,
+                               fmt::join(traces[traceIndex].samples, ", "));
         }
     }
 
     outputStream << oss.str().c_str();
-
-    qDebug() << __PRETTY_FUNCTION__ << "done, took" << sw.interval().count() / 1000.0 << "ms";
+    result.dtDecode = sw.interval();
 
     return result;
 }
 
-}
+} // namespace
 
 struct MdppSampleDecoderMonitorWidget::Private
 {
@@ -114,10 +119,9 @@ struct MdppSampleDecoderMonitorWidget::Private
 
 MdppSampleDecoderMonitorWidget::MdppSampleDecoderMonitorWidget(
     const std::shared_ptr<analysis::DataSourceMdppSampleDecoder> &source,
-    AnalysisServiceProvider *serviceProvider,
-    QWidget *parent)
-: QMainWindow(parent)
-, d(std::make_unique<Private>())
+    AnalysisServiceProvider *serviceProvider, QWidget *parent)
+    : QMainWindow(parent)
+    , d(std::make_unique<Private>())
 {
     d->q = this;
     d->ui = std::make_unique<Ui::MdppSampleDecoderMonitorWidget>();
@@ -126,57 +130,58 @@ MdppSampleDecoderMonitorWidget::MdppSampleDecoderMonitorWidget(
     d->asp_ = serviceProvider;
     d->refreshTimer_.setSingleShot(true);
 
-    for (auto tb: { d->ui->tb_input, d->ui->tb_output, d->ui->tb_log })
+    for (auto tb: {d->ui->tb_input, d->ui->tb_output, d->ui->tb_log})
     {
         tb->setReadOnly(true);
         tb->setFont(make_monospace_font());
     }
 
-    connect(d->ui->pb_pauseResumeRefresh, &QPushButton::clicked, this, [this] (bool checked)
-    {
-        if (checked)
-        {
-            d->ui->pb_pauseResumeRefresh->setText("Resume");
-        }
-        else
-        {
-            d->ui->pb_pauseResumeRefresh->setText("Pause");
-            d->refreshTimer_.start();
-        }
-    });
+    connect(d->ui->pb_pauseResumeRefresh, &QPushButton::clicked, this,
+            [this](bool checked)
+            {
+                if (checked)
+                {
+                    d->ui->pb_pauseResumeRefresh->setText("Resume");
+                }
+                else
+                {
+                    d->ui->pb_pauseResumeRefresh->setText("Pause");
+                    d->refreshTimer_.start();
+                }
+            });
 
     connect(d->ui->pb_refreshNow, &QPushButton::clicked, this, [this] { d->startRefresh(); });
 
-    connect(&d->refreshTimer_, &QTimer::timeout, this, [this]
-    {
-        if (!d->ui->pb_pauseResumeRefresh->isChecked())
-            d->startRefresh();
-    });
+    connect(&d->refreshTimer_, &QTimer::timeout, this,
+            [this]
+            {
+                if (!d->ui->pb_pauseResumeRefresh->isChecked())
+                    d->startRefresh();
+            });
 
-    const auto refreshIntervals = { 0, 100, 500, 1000 };
+    const auto refreshIntervals = {0, 100, 500, 1000};
 
     for (const auto &interval: refreshIntervals)
     {
         d->ui->combo_refreshInterval->addItem(
-            interval == 0 ? "off" : QString::number(interval) + " ms",
-            interval);
+            interval == 0 ? "off" : QString::number(interval) + " ms", interval);
     }
 
-    connect(d->ui->combo_refreshInterval, QOverload<int>::of(&QComboBox::currentIndexChanged),
-            this, [this] (int index)
-    {
-        const auto interval = d->ui->combo_refreshInterval->itemData(index).toInt();
-        d->refreshTimer_.setInterval(interval);
-        if (interval == 0)
-            d->refreshTimer_.stop();
-        else
-            d->refreshTimer_.start();
-    });
+    connect(d->ui->combo_refreshInterval, QOverload<int>::of(&QComboBox::currentIndexChanged), this,
+            [this](int index)
+            {
+                const auto interval = d->ui->combo_refreshInterval->itemData(index).toInt();
+                d->refreshTimer_.setInterval(interval);
+                if (interval == 0)
+                    d->refreshTimer_.stop();
+                else
+                    d->refreshTimer_.start();
+            });
 
     d->ui->combo_refreshInterval->setCurrentIndex(1);
 
-    connect(&d->debugResultWatcher_, &QFutureWatcher<DecoderDebugResult>::finished,
-        this, [this] { d->onRefreshDone(); });
+    connect(&d->debugResultWatcher_, &QFutureWatcher<DecoderDebugResult>::finished, this,
+            [this] { d->onRefreshDone(); });
 }
 
 MdppSampleDecoderMonitorWidget::~MdppSampleDecoderMonitorWidget()
@@ -225,7 +230,12 @@ void MdppSampleDecoderMonitorWidget::Private::onRefreshDone()
     else
         refreshTimer_.start();
 
-    qDebug() << __PRETTY_FUNCTION__ << "done, took" << sw.interval().count() / 1000.0 << "ms";
+    auto dtRefresh = sw.interval();
+
+    q->statusBar()->showMessage(
+        QString("Decode time: %1 ms, UI update time: %2 ms")
+            .arg(result.dtDecode.count() / 1000.0, 0, 'f', 1)
+            .arg(dtRefresh.count() / 1000.0, 0, 'f', 1));
 }
 
-}
+} // namespace analysis::ui
